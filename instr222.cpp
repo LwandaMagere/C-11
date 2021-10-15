37e7fb435d2c0787d872a454801b0b423eea00b2
LwandaMagere <dadolenssen@gmail.com>
2021-10-15 14:36:22 +0300

    r

---
+++ instr222.cpp
@@ -0,0 +1,19 @@
+// reading more than one word with getline
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    const int Arsize =  20;
+    char name[Arsize];
+    char dessert[Arsize];
+
+    cout << "Enter your name:\n";
+    cin.getline(name, Arsize); // reads through newline
+    cout << "Enter your favorite dessert:\n";
+    cin.getline(dessert, Arsize);
+    cout << "I have some delicious " <<  dessert;
+    cout << " for you, " << name << ".\n";
+
+    return 0;
+}
