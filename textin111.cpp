657626f6daefe236a0847340aa0d76342eebf9f2
LwandaMagere <dadolenssen@gmail.com>
2021-10-17 13:00:07 +0300

    r

---
+++ textin111.cpp
@@ -0,0 +1,20 @@
+// using cin.get(ch)
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    char ch;
+    int count = 0;
+
+    cout << "Enter characters; enter # to quit:\n";
+    cin.get(ch);
+    while (ch != '#')
+    {
+        cout << ch;
+        ++count;
+        cin.get(ch);
+    }
+    cout << endl << count << " characters read\n";
+    return 0;
+}
