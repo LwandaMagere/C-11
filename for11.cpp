6c3fb37fa11a6a6d95fd57496bff6df62bbbc5f3
LwandaMagere <dadolenssen@gmail.com>
2021-10-17 06:29:43 +0300

    for

---
+++ for11.cpp
@@ -0,0 +1,11 @@
+// introducing the for loop
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int i;
+    for (i = 1; i < 5; i++)
+        cout << "C++ knows loops.\n";
+    cout << "C++ knows when to stop.\n";
+    return 0;
+}
