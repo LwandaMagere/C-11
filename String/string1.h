eacd15a2a2da79881a4bfe59166fc18cf156bed1
LwandaMagere <dadolenssen@gmail.com>
2021-11-09 11:09:12 +0300

    r

---
+++ String/string1.h
@@ -0,0 +1,32 @@
+// fixed and augmented string class definition
+#ifndef STRING1_H_
+#define STRING1_H_
+#include <iostream>
+using std::ostream;
+using std::istream;
+
+class String
+{
+    private:
+        char * str;
+        int len;
+        static int num_strings;
+        static const in CINLIM = 80;
+    public:
+        String(const char * s);
+        String();
+        ~String();
+        int length() const { return len; }
+        String & operator=(const String &);
+        String & operator=(const char *);
+        char & operator[](int i);
+        const char & operator[](int i) const;
+        friend bool operator<(const String &st, const String &st2);
+        friend bool operator>(const String &st1, const String &st2);
+        friend bool operator==(const String &st, const String &st2);
+        friend istream & operator>>(istream & is, String & st);
+        friend ostream & operator<<(ostream & os, const String & st);
+        static int HowMany();
+};
+
+#endif
