9df61620981fa514f2d69820f169e0c720e72973
LwandaMagere <dadolenssen@gmail.com>
2021-11-09 23:11:40 +0300

    r

--- MOTO/queue.h
+++ MOTO/queue.h
@@ -5,13 +5,13 @@
 class Customer
 {
     private:
-        long arrive; // arrival time for customer
+        long arrive;  // arrival time for customer
         int processtime; // processing time for customer
     public:
-        Customer() { arrive = processtime = 0;}
+        Customer(){ arrive = processtime = 0; }
         void set(long when);
         long when() const { return arrive; }
-        int ptime() const { return processtime; }
+        int ptime() const { return processtime;}
 };
 
 typedef Customer Item;
@@ -19,29 +19,25 @@ typedef Customer Item;
 class Queue
 {
     private:
-
-        struct Node 
-        { 
-            Item item; 
+        stuct Node 
+        {
+            Item item;
             struct Node * next;
         };
-        
-        enum {Q_SIZE = 10};
-        Node * front; // pointer to front of the queue
+        enum { Q_SIZE =  10};
+        Node * front;
         Node * rear;
-        int items;   // current number of items in the queue
+        int items; // current number of items in Queue
         const int qsize; // maximum number of items in Queue
-
-        // preemptive definitions to prevent public copying
         Queue(const Queue & q) : qsize(0){}
-        Queue & operator=(const Queue & q){return *this;}
+        Queue & operator=(const Queue & q) { return *this;}
     public:
         Queue(int qs = Q_SIZE); // create queue with a qs limit
         ~Queue();
         bool isempty() const;
         bool isfull() const;
         int queuecount() const;
-        bool enqueue(const Item &item); 
+        bool enqueue(const Item & item); // add item to end
         bool dequeue(Item &item);
 };
 #endif
