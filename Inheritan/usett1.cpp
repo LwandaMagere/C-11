f898679b0b4f2383670b9fee5fbdd580f366f216
LwandaMagere <dadolenssen@gmail.com>
2021-11-13 19:55:31 +0300

    r

--- Inheritan/usett1.cpp
+++ Inheritan/usett1.cpp
@@ -7,10 +7,10 @@ int main()
     using std::cout;
     using std::endl;
     TableTennisPlayer player1("Tara", "Boomdea", false);
-    RatedPlayer rplayer(1140, "Malory", "Duck", true);
-    rplayer.Name(); // derived object uses base method
+    RatedPlayer rplayer1(1140, "Malory", "Duck", true);
+    rplayer1.Name(); // derived object uses base method
 
-    if (rplayer.HasTable())
+    if (rplayer1.HasTable())
         cout << ": has a table.\n";
     else 
         cout << ": hasn't a table.\n";
@@ -21,7 +21,7 @@ int main()
         cout << ": hasn't a table.\n";
     cout << "Name: ";
     rplayer1.Name();
-    cout << "; Rating: " << rplayer.Rating() << endl;
+    cout << "; Rating: " << rplayer1.Rating() << endl;
     // initialize RatedPlayer using TableTennisPlayer object
     RatedPlayer rplayer2(1212, player1);
     cout << "Name: ";
