e7d29f8984d5f2ed53482f9d2bd5716db9640acc
LwandaMagere <dadolenssen@gmail.com>
2021-11-13 20:47:43 +0300

    r

---
+++ sttring/strngbad.cpp
@@ -0,0 +1,40 @@
+#include <cstring>
+#include "strngbad.h"
+using std::cout;
+
+// initializing static class member
+int StringBad::num_strings = 0;
+
+StringBad::StringBad(const char * s)
+{
+    len = std::strlen(s);  // set size
+    str = new char[len + 1];  // allot storage
+    std::strcpy(str, s);
+    num_strings++;
+    cout << num_strings << ":\"" << str
+        << "\" object created\n";
+}
+
+StringBad::StringBad()
+{
+    len = 4;
+    str = new char[4];
+    std::strcpy(str, "C++");
+    num_strings++;
+    cout << num_strings << ": \"" << str    
+         << "\" default object created\n";
+}
+
+StringBad::~StringBad()
+{
+    cout << "\"" << str << "\" object deleted, ";
+    --num_strings;
+    cout << num_strings << " left\n";
+    default [] str;
+}
+
+std::ostream & operator<<(std::ostream & os, const StringBad & st)
+{
+    os << st.str;
+    return os;
+}
