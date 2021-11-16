d2d2fe77ec46894cbad9c1136f8d0ca68fcab4ab
LwandaMagere <dadolenssen@gmail.com>
2021-11-16 16:39:16 +0300

    r

--- Compound/instr2.cpp
+++ Compound/instr2.cpp
@@ -8,14 +8,16 @@ int main()
     using std::cin;
     using std::cout;
 
-    string name1, name2;
+  string name1, name2;
 
     cout << "What is your name?\n";
     cin >> name1;
-   // cout << "What is your second name?\n";
-   // cin >> name2;
+    
+    cout << "What is your second name?\n";
+    cin >> name2;
+    
 
-    cout << "Your name is" << name1 << std::endl;// << name2 << std::endl;
+    cout << "Your name is " << name1 << " "  << name2 << std::endl;
 
     
     return 0;
