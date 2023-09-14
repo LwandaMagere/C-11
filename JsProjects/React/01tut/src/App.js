5eb5262ac76ca66a4a19a706b1ade78109cbfc85
LwandaMagere <dadolenssen@gmail.com>
2023-09-15 00:08:39 +0300

    r

--- JsProjects/React/01tut/src/App.js
+++ JsProjects/React/01tut/src/App.js
@@ -1,32 +1,16 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
 
-      const handleNamechange = () => {
-          const names = ['Dado', 'Justin', 'Gwiji'];
-          const int = Math.floor(Math.random() * 3);
-          return names[int];
-      }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           Hello {handleNamechange()}!
-        </p>
-
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-
-      </header>
+      <Header/>
+      <Content/>
+      <Footer />  
     </div>
   );
 }
