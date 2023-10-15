794fc236b22b6daaee9bf2bf537e82782ba8a174
LwandaMagere <dadolenssen@gmail.com>
2023-10-15 12:30:35 +0300

    r

--- mzitoJS/JS/app.js
+++ mzitoJS/JS/app.js
@@ -1,15 +1,76 @@
-// BASICS
+// //BASICS
 
-    // // alert("Mzito Js");
-    // console.table({a: 1, b: 2});
-    // console.error("This is some error");
-    // console.clear();
-    // console.warn("This is some warning");
-    // console.time("Hello");
-    //     console.log("Hello World!");
-    //     console.log("Hello World!");
-    //     console.log("Hello World!");
-    //     console.log("Hello World!");
-    //     console.log("Hello World!");
-    // console.timeEnd("Hello");
+//     alert("Mzito Js");
+//     console.table({a: 1, b: 2});
+//     console.error("This is some error");
+//     console.clear();
+//     console.warn("This is some warning");
+//     console.time("Hello");
+//         console.log("Hello World!");
+//         console.log("Hello World!");
+//         console.log("Hello World!");
+//         console.log("Hello World!");
+//         console.log("Hello World!");
+//     console.timeEnd("Hello");
+
+
+//////////////////////////////////////////////////////
+// const name = 'John';
+// const age = 30;
+// const job = 'Software Engineer';
+// const city = 'Dagoreti';
+// let html;
+
+// function hello() {
+//     return 'Hello!';
+// }
+
+// html = `
+//     <ul>
+//         <li>Name: ${name}</li>
+//         <li>Age: ${age}</li>
+//         <li>Job: ${job}</li>
+//         <li>City: ${city}</li>
+//         <li>${hello()}</li>
+//     </ul>
+// `;
+
+// document.body.innerHTML = html;
+///////////////////////////////////////////////////////////
+
+// numbers = new Array(23, 20, 45, 56, 73, 46, 83, 34);
+// let val = numbers.sort();
+// console.log(val);
+// val = numbers.reverse();
+// console.log(val);
+
+// function under50(num) {
+//     return num < 50;
+// }
+
+// val = numbers.find(under50);
+// console.log(val);
+///////////////////////////////////////////////////////////
+
+// (function(name){
+//     console.log('Hi ' + name);
+// })('Dado');
+//////////////////////////////////////////////////////////
+
+// const users = [
+//     {id: 1, name: 'Dado'},
+//     {id: 2, name: 'Jopadhola'},
+//     {id: 3, name: 'Jokajock'}
+// ];
+
+// const ids = users.map(function(user){
+//     return user.id;
+// });
+// const names = users.map(function(name1){
+//     return name1.name;
+// })
+
+// console.log(ids);
+// console.log(names);
+////////////////////////////////////////////////////////////
 
