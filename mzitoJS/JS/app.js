eb47f3f9fcb9f60fa3587f630d6e1edf6f9c933c
LwandaMagere <dadolenssen@gmail.com>
2023-10-18 16:44:17 +0300

    r

--- mzitoJS/JS/app.js
+++ mzitoJS/JS/app.js
@@ -10,37 +10,121 @@
     //     console.log('Hello World!');
     // console.timeEnd('Hello');
 
-// **%%$$@@&& key value pair
+// **%%$$@@&& key value pair **%%$$@@&&
 
 
 // ********** 1. DATA TYPES ***************************
 
     // 1. Primitives
 
-    // String
-    const name = 'Dado Steven';
-    // Number
-    const age = 32;
-    // Boolean
-    const hasKids = false;
-    // Null
-    const car = null;
-    // Undefined
-    let test;
-    // Symbol
-    const sym = Symbol();
+    // // String
+    // const name = 'Dado Steven';
+    // // Number
+    // const age = 32;
+    // // Boolean
+    // const hasKids = false;
+    // // Null
+    // const car = null;
+    // // Undefined
+    // let test;
+    // // Symbol
+    // const sym = Symbol();
 
-    // 2. Reference Type - Objects
+    // // 2. Reference Type - Objects
 
-    // Arrays
-    const hobbies = ['movies', 'music'];
-    // Object literal
-    const address = {
-        city: 'Nairobi',
-        State: 'Kenya'
-    };
-    // Dates
-    const today = new Date();
+    // // Arrays
+    // const hobbies = ['movies', 'music'];
+    // // Object literal
+    // const address = {
+    //     city: 'Nairobi',
+    //     State: 'Kenya'
+    // };
+    // // Dates
+    // const today = new Date();
+
+    // console.log(typeof today);
 
 
-    console.log(typeof today);
+    // ********** 2. TYPE CONVERSION ***************************
+
+        // let val;
+
+        // // Number to String
+        // val = String(5);
+        // val = String(4 + 5);
+        // // Bool to String
+        // val = String(true);
+        // // Date to String
+        // val = String(new Date());
+        // // Array to String
+        // val = String([1,2,3,4]);
+
+        // // toString()
+        // val = (44).toString();
+        // val = (true).toString();
+
+        // // String to Numbers
+        // val = Number('4');
+        // val = Number(true);
+        // val = Number(null);
+        // val = Number('Hello');
+        // val = Number([1,2,3,4]);
+
+        // val = parseInt('100');
+        // val = parseFloat('100.30');
+
+        // // Type coersion Js does it for us.
+        // const val1 = String(5);
+        // const val2 = 6;
+        // const sum = val1 + val2;
+
+
+        // console.log(sum);
+        // console.log(typeof sum);
+        // console.log(sum.length);
+        // console.log(val.toFixed(2))
+
+        // -- Property is like an attribute
+
+// ********** 3. STRING METHODS  ***************************
+
+    const firstName = 'Dado';
+    const lastName = 'Steven';
+    const str = 'Hey my name is Steven Dado Lenssen';
+    const tags = 'web design,web development,programming';
+
+    let val;
+    // Concatenation
+    val = firstName + ' ' + lastName;
+    // Append
+    val = 'Lennsen ';
+    val += 'Steven';
+    // Escaping ***
+    // Length --- it's a property and not a method
+    // Concat
+    val = firstName.concat('***', lastName);
+    //Change case --- toUpperCase ...
+    // indexOf
+    val = lastName.indexOf('e'); //gives -1 if the character isn't there
+    // charAt()
+    val = lastName.charAt('2');
+    // get last character
+    val = lastName.charAt(lastName.length - 1);
+    // Substring
+    val = lastName.substring(0, 4);
+    // Slice
+    val = lastName.slice(0, 4);
+    val = lastName.slice(-3);
+    // Split
+    val = str.split('');
+    val = str.split(' ');
+    val = str.split('  ');
+    val = tags.split(',');
+    // Replace
+    val = str.replace('Steven', 'Lwanda');
+    val = str.includes('ste');
+
+    console.log(val);
+
+
+        
