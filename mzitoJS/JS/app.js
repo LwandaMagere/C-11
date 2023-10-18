51b9c8d3ec8abe787dbf1face29206a1da888c67
LwandaMagere <dadolenssen@gmail.com>
2023-10-18 15:35:50 +0300

    r

--- mzitoJS/JS/app.js
+++ mzitoJS/JS/app.js
@@ -9,3 +9,38 @@
     //     console.log('Hello World!');
     //     console.log('Hello World!');
     // console.timeEnd('Hello');
+
+// **%%$$@@&& key value pair
+
+
+// ********** 1. DATA TYPES ***************************
+
+    // 1. Primitives
+
+    // String
+    const name = 'Dado Steven';
+    // Number
+    const age = 32;
+    // Boolean
+    const hasKids = false;
+    // Null
+    const car = null;
+    // Undefined
+    let test;
+    // Symbol
+    const sym = Symbol();
+
+    // 2. Reference Type - Objects
+
+    // Arrays
+    const hobbies = ['movies', 'music'];
+    // Object literal
+    const address = {
+        city: 'Nairobi',
+        State: 'Kenya'
+    };
+    // Dates
+    const today = new Date();
+
+
+    console.log(typeof today);
