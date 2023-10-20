8f7421215e6ef756d130884372c65ba858eef220
LwandaMagere <dadolenssen@gmail.com>
2023-10-20 13:37:26 +0300

    r

--- mzitoJS/JS/03_DOM/app.js
+++ mzitoJS/JS/03_DOM/app.js
@@ -111,90 +111,185 @@
 // ..........................................................................................................
 
 // Traversing the DOM
-let val;
+// let val;
 
-const list = document.querySelector('ul.collection');
-const listItem = document.querySelector('li.collection-item:first-child');
+// const list = document.querySelector('ul.collection');
+// const listItem = document.querySelector('li.collection-item:first-child');
 
-val = listItem;
-val = list;
+// val = listItem;
+// val = list;
 
-// Get child nodes -- this includes the text(spaces) -- returns node list
-val  = list.childNodes;
-val  = list.childNodes[1];
-val  = list.childNodes[0].nodeName;
-val  = list.childNodes[1].nodeType; // 1 2 3 8 9 10
+// // Get child nodes -- this includes the text(spaces) -- returns node list
+// val  = list.childNodes;
+// val  = list.childNodes[1];
+// val  = list.childNodes[0].nodeName;
+// val  = list.childNodes[1].nodeType; // 1 2 3 8 9 10
 
-/**
- * 1.Element node
- * 2.Attribute (deprected)
- * 3.Text node
- * 8.Comment
- * 9.Document itself
- * 10.Doctype
- */
+// /**
+//  * 1.Element node
+//  * 2.Attribute (deprected)
+//  * 3.Text node
+//  * 8.Comment
+//  * 9.Document itself
+//  * 10.Doctype
+//  */
 
-// Get the children element nodes -- this doesn't include text(space) -- returns the html collection
-// val = list.children;
+// // Get the children element nodes -- this doesn't include text(space) -- returns the html collection
+// // val = list.children;
 
-// val = list.children[3].children[0];
-// // first child
-// val = list.firstChild;
-// val = list.firstElementChild;
+// // val = list.children[3].children[0];
+// // // first child
+// // val = list.firstChild;
+// // val = list.firstElementChild;
 
-// // last child
-// val = list.lastChild;
-// val = list.lastElementChild;
+// // // last child
+// // val = list.lastChild;
+// // val = list.lastElementChild;
 
-// // Count child element
-// val = list.childElementCount;
+// // // Count child element
+// // val = list.childElementCount;
 
-// // Get  parent nodes
-// val = listItem.parentNode;
-// val = listItem.parentElement;
-// val = listItem.parentElement.parentElement;
+// // // Get  parent nodes
+// // val = listItem.parentNode;
+// // val = listItem.parentElement;
+// // val = listItem.parentElement.parentElement;
 
-// // Get next sibling
-// val = listItem.nextSibling;
-// val = listItem.nextElementSibling;
-// val = listItem.nextElementSibling.nextElementSibling;
+// // // Get next sibling
+// // val = listItem.nextSibling;
+// // val = listItem.nextElementSibling;
+// // val = listItem.nextElementSibling.nextElementSibling;
 
-// // Get prev sibling
-// val = listItem.previousSibling;
-// val = listItem.nextElementSibling;
-// val = listItem.previousElementSibling;
+// // // Get prev sibling
+// // val = listItem.previousSibling;
+// // val = listItem.nextElementSibling;
+// // val = listItem.previousElementSibling;
+
+// // console.log(val);
+
+// // CREATING ELEMENTS
+
+// // Creat elements
+// const li = document.createElement('li');
+
+// // Add class
+// li.className = 'collection-item';
+// // Add id
+// li.id = 'new-item';
+// // Add Attribute
+// li.setAttribute('title', 'New Item');
+
+// // Create text node and append
+// li.appendChild(document.createTextNode('Hello World'));
+// // Append li as child to ul
+// document.querySelector('ul.collection').appendChild(li);
+
+// // Create link
+// const link = document.createElement('a');
+// // Add class
+// link.className = 'delete-item secondary-content';
+// // Add icon html
+// link.innerHTML = '<i class="fa fa-remove"></i>';
+// // Append link to li
+// li.appendChild(link);
+// // Append li as child to ul
+// document.querySelector('ul.collection').appendChild(li);
+
+// console.log(li);
+
+// ..........................................................................................................
+
+// REMOVING AND REPLACING ELEMENTS
+
+// Create element
+// const newHeading = document.createElement('h2');
+// // Add id
+// newHeading.id = 'task-title';
+// // New text node
+// newHeading.appendChild(document.createTextNode('Task List'));
+
+// // Get the old heading
+// const oldHeading = document.getElementById('task-title');
+// // Parent
+// cardAction  = document.querySelector('.card-action');
+
+// // Replace
+// cardAction.replaceChild(newHeading, oldHeading);
+
+// // Remove element
+// const lis = document.querySelectorAll('li');
+// const list = document.querySelector('ul');
+
+// // Remove list item
+// lis[0].remove();
+// // Remove child node
+// list.removeChild(lis[3]);
+
+// CLASSES & ATTR
+// const firstLi = document.querySelector('li:first-child');
+// const link = firstLi.children[0];
+
+// let val;
+
+// // Classes
+// val = link.className;
+// val = link.classList;
+// val = link.classList[0];
+// link.classList.add('test');
+// link.classList.remove('test');
+// val = link;
+
+// // Attribute
+// val = link.getAttribute('href');
+// val = link.setAttribute('href', 'http://google.com');
+// val = link.setAttribute('title','Google');
+// val = link.hasAttribute('title');
+// link.removeAttribute('title');
+// val = link;
 
 // console.log(val);
 
-// CREATING ELEMENTS
+// EVENT LISTENERS
 
-// Creat elements
-const li = document.createElement('li');
+// document.querySelector('.clear-tasks').addEventListener('click', function(e){
+//     console.log('Hello World!');
 
-// Add class
-li.className = 'collection-item';
-// Add id
-li.id = 'new-item';
-// Add Attribute
-li.setAttribute('title', 'New Item');
+//     e.preventDefault();
+// });
 
-// Create text node and append
-li.appendChild(document.createTextNode('Hello World'));
-// Append li as child to ul
-document.querySelector('ul.collection').appendChild(li);
+document.querySelector('.clear-tasks').addEventListener('click', onClick);
 
-// Create link
-const link = document.createElement('a');
-// Add class
-link.className = 'delete-item secondary-content';
-// Add icon html
-link.innerHTML = '<i class="fa fa-remove"></i>';
-// Append link to li
-li.appendChild(link);
-// Append li as child to ul
-document.querySelector('ul.collection').appendChild(li);
+function onClick(a) {
+    console.log('Clicked...');
 
-console.log(li);
+    let val;
+    val = a; // e is the event object
+
+    // Event target element
+    val = a.target;
+    val = a.target.id;
+    val = a.target.className;
+    val = a.target.classList;
+
+    // a.target.innerText = 'Hello';
+
+    //Event Type
+    val = a.type;
+    // TimeStamp
+    val = a.timeStamp;
+    // Coordinates of the events relative to the window
+    val = a.clientY;
+    val = a.clientX;
+     // Coordinates of the events relative to the element
+     val = a.offsetY;
+     val = a.offsetX;
+
+    console.log(val);
+
+
+    a.preventDefault();
+}
+
+
 
 
 
