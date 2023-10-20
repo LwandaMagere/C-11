4edbb538254e5975380f713c250d867b29b1cb76
LwandaMagere <dadolenssen@gmail.com>
2023-10-20 11:24:11 +0300

    r

--- mzitoJS/JS/03_DOM/app.js
+++ mzitoJS/JS/03_DOM/app.js
@@ -93,20 +93,109 @@
 
 // 2. document.querySelectorAll() node list
 
-const items = document.querySelectorAll('ul.collection li.collection-item');
+// const items = document.querySelectorAll('ul.collection li.collection-item');
 
-items.forEach(function(item, index){
-    item.textContent = `${index}: Hello`;
-});
+// items.forEach(function(item, index){
+//     item.textContent = `${index}: Hello`;
+// });
 
-const liOdd = document.querySelectorAll('li:nth-child(odd)');
-const liEven = document.querySelectorAll('li:nth-child(even)');
+// const liOdd = document.querySelectorAll('li:nth-child(odd)');
+// const liEven = document.querySelectorAll('li:nth-child(even)');
 
-liOdd.forEach(function(li, index){
-    li.style.background = '#ccc';
-})
+// liOdd.forEach(function(li, index){
+//     li.style.background = '#ccc';
+// })
 
-console.log(items);
+// console.log(items);
+
+// ..........................................................................................................
+
+// Traversing the DOM
+let val;
+
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+
+val = listItem;
+val = list;
+
+// Get child nodes -- this includes the text(spaces) -- returns node list
+val  = list.childNodes;
+val  = list.childNodes[1];
+val  = list.childNodes[0].nodeName;
+val  = list.childNodes[1].nodeType; // 1 2 3 8 9 10
+
+/**
+ * 1.Element node
+ * 2.Attribute (deprected)
+ * 3.Text node
+ * 8.Comment
+ * 9.Document itself
+ * 10.Doctype
+ */
+
+// Get the children element nodes -- this doesn't include text(space) -- returns the html collection
+// val = list.children;
+
+// val = list.children[3].children[0];
+// // first child
+// val = list.firstChild;
+// val = list.firstElementChild;
+
+// // last child
+// val = list.lastChild;
+// val = list.lastElementChild;
+
+// // Count child element
+// val = list.childElementCount;
+
+// // Get  parent nodes
+// val = listItem.parentNode;
+// val = listItem.parentElement;
+// val = listItem.parentElement.parentElement;
+
+// // Get next sibling
+// val = listItem.nextSibling;
+// val = listItem.nextElementSibling;
+// val = listItem.nextElementSibling.nextElementSibling;
+
+// // Get prev sibling
+// val = listItem.previousSibling;
+// val = listItem.nextElementSibling;
+// val = listItem.previousElementSibling;
+
+// console.log(val);
+
+// CREATING ELEMENTS
+
+// Creat elements
+const li = document.createElement('li');
+
+// Add class
+li.className = 'collection-item';
+// Add id
+li.id = 'new-item';
+// Add Attribute
+li.setAttribute('title', 'New Item');
+
+// Create text node and append
+li.appendChild(document.createTextNode('Hello World'));
+// Append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+
+// Create link
+const link = document.createElement('a');
+// Add class
+link.className = 'delete-item secondary-content';
+// Add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+// Append link to li
+li.appendChild(link);
+// Append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+
+console.log(li);
+
 
 
 
