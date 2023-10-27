e8abebc9ae954fcb18b463ff5d135b8cc8a7d919
LwandaMagere <dadolenssen@gmail.com>
2023-10-27 15:47:08 +0300

    r

--- mzitoJS/JS/04_EventBubling/app.js
+++ mzitoJS/JS/04_EventBubling/app.js
@@ -40,7 +40,65 @@
 //     // }
 // }
 
-// LOCAL STORAGE
+// LOCAL STORAGE  -- the local storage API -- what you set as a value has to be a string
+
+// set local storage item -- will stay untill u manually clear it out
+// localStorage.setItem('name', 'Dado');
+// localStorage.setItem('age', '30');
+
+// // set session storage item -- goes away when you close the browser
+// sessionStorage.setItem('name', 'Steven');
+
+// // remove from the storage
+// // localStorage.removeItem('name');
+
+// // get from storage
+// const name = localStorage.getItem('name');
+// const age  = localStorage.getItem('age');
+// console.log(name);
+
+// // clear local storage
+// localStorage.clear();
+
+// console.log(name, age);
+
+// document.querySelector('form').addEventListener('submit', function(e) {
+//     const task = document.getElementById('task').value;
+//     console.log(task);
+//     localStorage.setItem('task', task);
+//     alert('task saved')
+
+//     e.preventDefault();
+// })........1
+
+document.querySelector('form').addEventListener('submit', function(e){
+
+    const task = document.getElementById('task').value;
+
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task)
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    // alert('Task Saved');
+
+    e.preventDefault();
+})
+
+const tasks  = JSON.parse(localStorage.getItem('tasks'));
+tasks.forEach(function(task){
+    console.log(task);
+})
+
+
+
 
 
 
