da2636c91a0a78bc5fc0531d2a9fe1430508e909
LwandaMagere <dadolenssen@gmail.com>
2023-10-27 14:28:06 +0300

    r

---
+++ mzitoJS/JS/04_EventBubling/app.js
@@ -0,0 +1,29 @@
+// EVENT BUBBLING - The bubbling up of events through the DOM
+
+// document.querySelector('.card-title').addEventListener('click', function(){
+//     console.log('card title');
+// });
+
+// document.querySelector('.card-content').addEventListener('click', function(){
+//     console.log('card content');
+// });
+
+// document.querySelector('.card').addEventListener('click', function(){
+//     console.log('card ');
+// });
+
+// document.querySelector('.col').addEventListener('click', function(){
+//     console.log('col');
+// });
+
+// EVENT DELAGATION
+
+// const delItem = document.querySelector('.delete-item');
+
+// delItem.addEventListener('click', deleteItem);
+
+function deleteItem() {
+    console.log('delete item');
+}
+
+
