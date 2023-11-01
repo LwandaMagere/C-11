f5759617bded8971631dc01791e1e35ee3221bcd
LwandaMagere <dadolenssen@gmail.com>
2023-11-01 10:56:09 +0300

    r

--- mzitoJS/JS/03_DOM/app.js
+++ mzitoJS/JS/03_DOM/app.js
@@ -39,6 +39,48 @@
 // })
 
 // console.log(val);
+//..................................................................................................................................
+
+// DOM SELECTORS - This are document object methods that allows us to pull things from the DOM. Selection and manipulation
+
+// Single Element Selector getELementById & querySelector
+
+// getElementById() - Only select by ID ..................................
+
+// console.log(document.getElementById('task-title'));
+
+// // Get things from the element
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
+
+// // Change styling
+// document.getElementById('task-title').style.background = "#333";
+// document.getElementById('task-title').style.color = '#fff';
+// document.getElementById('task-title').style.padding = '5px';
+
+// // Change content
+// document.getElementById('task-title').textContent = 'Task List';
+// document.getElementById('task-title').innerText = 'My Tasks';
+// document.getElementById('task-title').innerHTML = '<span style= "color:red">Task List</span>';
+
+// document.querySelector() ................................................
+
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
+
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'blue';
+
+// document.querySelector('li:last-child').style.color = 'red';
+// document.querySelector('li:nth-child(3)').style.color = 'gold';
+// document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+// document.querySelector('li:nth-child(odd)').style.background = "#ccc";
+// document.querySelector('li:nth-child(even)').style.background = '#f4f4'
+// .............................................................................
+
+
+// DOM Selector for Multiple Elements
 
 
 
