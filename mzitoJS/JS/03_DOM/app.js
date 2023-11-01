a7cca9c215e2457f9a2972d62b8eff1a0c1e9256
LwandaMagere <dadolenssen@gmail.com>
2023-11-01 23:29:02 +0300

    r

--- mzitoJS/JS/03_DOM/app.js
+++ mzitoJS/JS/03_DOM/app.js
@@ -82,6 +82,61 @@
 
 // DOM Selector for Multiple Elements
 
+// document.getELementsByClassName()........................................
+
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello Steve';
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// console.log(listItems);
+
+// document.getELementsByTagName() An HTMLCollection is not an Array.............
+
+// let lis = document.getElementsByTagName('li');
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = 'red';
+// lis[1].textContent = 'Hello';
+
+// // Convert HTML Collection into an Array
+// lis = Array.from(lis);
+// lis.reverse();
+
+// lis.forEach(function(li, index){
+//     console.log(li.className);
+//     li.textContent = `${index} : Hello`;
+// })
+
+// console.log(lis);
+
+// document.querySelectorAll() they are the same as the two above except it returns node list which counts things like text nodes that are spaces, it allows array methods and forEach without conversion................................................ you can still use length wiht an html collection...........................
+
+// const items = document.querySelectorAll('ul.collection li.collection-item');
+
+// items.forEach(function(item, index){
+//     item.textContent = `${index} : Dado`
+// })
+
+// const liOdd = document.querySelectorAll('li:nth-child(odd)');
+// const liEven = document.querySelectorAll('li:nth-child(even)');
+
+// liOdd.forEach(function(li, index){
+//     li.style.background = '#ccc';
+// })
+
+// for (let i = 0; i < liEven.length; i++){
+//     liEven[i].style.background = '#f4f4';
+// }
+
+// console.log(items);
+
+//  TRAVERSING THE DOM ..............................................
+
+
+
 
 
 
