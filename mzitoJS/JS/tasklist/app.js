f278d05819352114b1cd0392f0ef22f4ca7d4780
LwandaMagere <dadolenssen@gmail.com>
2023-11-03 10:45:26 +0300

    r

---
+++ mzitoJS/JS/tasklist/app.js
@@ -0,0 +1,45 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListener();
+
+// Load all event listeners
+function loadEventListener() {
+    // Add task event
+    form.addEventListener('submit', addTask);
+}
+
+// Add Task
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('Add Task!');
+    }
+
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to the li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+
+    // Append the li to the ul
+    taskList.appendChild(li);
+
+    // Clear input
+    taskList.value = '';
+
+    e.preventDefault();
+}
