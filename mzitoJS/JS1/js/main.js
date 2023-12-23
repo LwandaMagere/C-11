54822758461587c5e6e5edd6d4efda2004308c1d
LwandaMagere <dadolenssen@gmail.com>
2023-12-23 23:42:36 +0300

    r

--- mzitoJS/JS1/js/main.js
+++ mzitoJS/JS1/js/main.js
@@ -1,19 +1,27 @@
 // Intro
-console.log("Hello Block!");
-console.log(typeof "Dado");
+    // console.log("Hello Block!");
+    // console.log(typeof "Dado");
 
 // String
-const myVariable = "Mathematics";
+    // const myVariable = "Mathematics";
 
 // The length property
-console.log(myVariable.length);
-console.log("Every good boy does just fine".length);
+    // console.log(myVariable.length);
+    // console.log("Every good boy does just fine".length);
 
 // String Method
-console.log(myVariable.charAt(0));
-console.log(myVariable.indexOf('Mat'));
-console.log(myVariable.lastIndexOf('at'));
-console.log(myVariable.slice(5, 8));
-console.log(myVariable.toUpperCase());
-console.log(myVariable.includes("mat"));
-console.log(myVariable.split("e"));
+    // console.log(myVariable.charAt(0));
+    // console.log(myVariable.indexOf('Mat'));
+    // console.log(myVariable.lastIndexOf('at'));
+    // console.log(myVariable.slice(5, 8));
+    // console.log(myVariable.toUpperCase());
+    // console.log(myVariable.includes("mat"));
+    // console.log(myVariable.split("e"));
+
+// Code Challenge
+
+// Write code that will return a random letter from your name
+// console.log("Dado".charAt(Math.floor(Math.random() * 4)));
+
+const anyName = "Steven";
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
