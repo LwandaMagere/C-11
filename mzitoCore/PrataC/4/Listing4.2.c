25957441ad48bb4de68705d402bb58d3b43496c1
LwandaMagere <dadolenssen@gmail.com>
2024-01-02 10:16:23 +0300

    r

---
+++ mzitoCore/PrataC/4/Listing4.2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define PRAISE "You are an extraodinary being."
+int main(void) 
+{
+    char name[40];
+
+    printf("What's your name? ");
+    scanf("%s", name);
+    printf("Hello, %s. %s\n", name, PRAISE);
+    
+    return 0;
+}
