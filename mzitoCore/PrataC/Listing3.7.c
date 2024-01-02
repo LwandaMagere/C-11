3b861eabfc7f072a9490cb8ff49f8fe970ee922f
LwandaMagere <dadolenssen@gmail.com>
2024-01-02 09:27:58 +0300

    r

---
+++ mzitoCore/PrataC/Listing3.7.c
@@ -0,0 +1,14 @@
+// displays float value in two ways
+#include <stdio.h>
+int main(void) 
+{
+    float aboat = 32000.0;
+    double abet = 2.14e9;
+    long double dip = 5.32e-5;
+
+    printf("%f can be written %e\n", aboat, aboat);
+    printf("And it's %a in hexadecimal, powers of 2 notation\n", aboat);
+    printf("%f can be written %e\n", abet, abet);
+    
+    return 0;
+}
