e0b2fc1544059188a4a2a449c55dec5f3767bbb8
LwandaMagere <dadolenssen@gmail.com>
2024-01-06 20:29:01 +0300

    r

--- mzitoCore/PrataC/8/Listing8.4.c
+++ mzitoCore/PrataC/8/Listing8.4.c
@@ -8,7 +8,7 @@ int main(void)
     printf("it.\nRespond with a y if my guess is right and with");
     printf("\nan n if it is wrong.\n");
     printf("Uh...is your number %d?\n", guess);
-    while (getchar() != 'y')
+    while (getchar() != 'y')  /* get response, compare to y */
         printf("Well, then, is it %d?\n", ++guess);
     printf("I knew I could do it!\n");
     
