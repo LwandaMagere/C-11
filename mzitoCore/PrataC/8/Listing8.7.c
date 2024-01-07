463779e550f6ab80a918cf0c071a4a4f29cc916e
LwandaMagere <dadolenssen@gmail.com>
2024-01-07 23:26:00 +0300

    r

--- mzitoCore/PrataC/8/Listing8.7.c
+++ mzitoCore/PrataC/8/Listing8.7.c
@@ -45,3 +45,54 @@ int main(void)
 
     return 0;
 }
+
+long get_long(void) 
+{
+    long input;
+    char ch;
+
+    while (scanf("%ld", &input) != 1)
+    {
+        while ((ch = getchar()) != '\n')
+            putchar(ch);  // dispose of bad input
+        printf(" is not an integer.\nPlease enter an ");
+        printf("integer value, such as 25, -178, or 3: ");
+    }
+
+    return input;
+}
+
+double sum_squares(long a, long b)
+{
+    double total = 0;
+    long i;
+
+    for (i = a; i <= b; i++)
+        total += (double)i * (double)i;
+
+    return total;
+}
+
+bool bad_limits(long begin, long end, long low, long high) 
+{
+    bool not_good = false;
+
+    if (begin > end) 
+    {
+        printf("%ld isn't smaller than %ld.\n", begin, end);
+        not_good = true;
+    }
+    if (begin < low || end < low) 
+    {
+        printf("Values must be %ld or greater.\n", low);
+        not_good = true;
+    }
+
+    if (begin > high || end > high)
+    {
+        printf("Values must be %ld or less.\n", high);
+        not_good = true;
+    }
+    
+    return not_good;
+}
