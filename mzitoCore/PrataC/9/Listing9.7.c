b8cf4a2317609cb5900823d9aede1228402c8c9c
LwandaMagere <dadolenssen@gmail.com>
2024-01-08 12:02:07 +0300

    r

--- mzitoCore/PrataC/9/Listing9.7.c
+++ mzitoCore/PrataC/9/Listing9.7.c
@@ -27,3 +27,25 @@ int main(void)
 
     return 0;
 }
+
+long fact(int n)  // loop-based function
+{
+    long ans;
+
+    for (ans = 1; n > 1; n--)
+        ans *= n;
+
+    return ans;
+}
+
+long rfact(int n) 
+{
+    long ans;
+
+    if (n > 0)
+        ans = n * rfact(n-1);
+    else
+        ans = 1;
+    
+    return ans;
+}
