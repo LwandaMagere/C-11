deac151faa9e0c6c1c0b1a9a9182d8046ae3935f
LwandaMagere <dadolenssen@gmail.com>
2024-01-17 22:43:29 +0300

    r

--- mzitoCore/PrataC/11/Listing11.8.c
+++ mzitoCore/PrataC/11/Listing11.8.c
@@ -33,5 +33,9 @@ int main(void)
     puts("Enter strings (empty line to quit):");
     while (fgets(words, STLEN, stdin))    ;
         puts("Done");
+
+    puts("Enter strings (empty line to quit):");
+    while (fgets(words, STLEN, stdin))
+        puts("Done");
     return 0;
 }
