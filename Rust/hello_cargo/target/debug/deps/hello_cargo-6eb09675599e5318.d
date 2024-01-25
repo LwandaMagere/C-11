e107da7b6265005eee5f5f1fe91130a1589a119c
LwandaMagere <dadolenssen@gmail.com>
2024-01-25 13:43:49 +0300

    r

---
+++ Rust/hello_cargo/target/debug/deps/hello_cargo-6eb09675599e5318.d
@@ -0,0 +1,5 @@
+/home/lwanda/Desktop/js_sandbox/Rust/hello_cargo/target/debug/deps/hello_cargo-6eb09675599e5318: src/main.rs
+
+/home/lwanda/Desktop/js_sandbox/Rust/hello_cargo/target/debug/deps/hello_cargo-6eb09675599e5318.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/lwanda/Desktop/js_sandbox/Rust/hello_cargo/target/debug/hello_cargo: /home/lwanda/Desktop/js_sandbox/Rust/hello_cargo/src/main.rs
