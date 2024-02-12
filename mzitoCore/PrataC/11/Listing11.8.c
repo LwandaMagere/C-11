26d3170b41627ea05a99197a4bc1aa4c1e73ce81
LwandaMagere <dadolenssen@gmail.com>
2024-02-12 05:33:08 +0300

    r

--- mzitoCore/PrataC/11/Listing11.8.c
+++ mzitoCore/PrataC/11/Listing11.8.c
@@ -1,41 +1,14 @@
 /* fgets2.c -- using fgets() and fputs() */
 #include <stdio.h>
 #define STLEN 10
-int main(void)
+int main(void) 
 {
     char words[STLEN];
 
-    // puts("Enter strings (empty line to quit):");
-    // while (fgets(words, STLEN, stdin) != NULL && words[0] != '\n')
-    //     fputs(words, stdout);
-    // puts("Done.");
-
-    puts("Enter strings (empty line to quit):");
-    while (fgets(words, STLEN, stdin));
-        puts("Done");
-
-    puts("Enter anything you feel like bana");
-    while (fgets(words, STLEN, stdin));
-        puts("Done");
-    
-    puts("Enter Strings (Empty line to quit):");
-    while (fgets(words, STLEN, stdin));
-        puts("Done ");
-
-    puts("Enter strings (empty line to quit):");
-    while (fgets(words, STLEN, stdin));
-        puts("Done");
-
-    puts("Enter strings (empty line to quit):");
-    while (fgets(words, STLEN, stdin));
-        puts("Done");
-
-    puts("Enter strings (empty line to quit):");
-    while (fgets(words, STLEN, stdin))    ;
-        puts("Done");
-
     puts("Enter strings (empty line to quit):");
     while (fgets(words, STLEN, stdin))
-        puts("Done");
+        fputs(words, stdout);
+    puts("Done");
+    
     return 0;
 }
