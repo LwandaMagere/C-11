56893fbebebb834a3ee0399ad291b057798ee8d4
LwandaMagere <dadolenssen@gmail.com>
2024-02-20 11:08:14 +0300

    r

--- mzitoCore/PrataC/11/Listing11.1.c
+++ mzitoCore/PrataC/11/Listing11.1.c
@@ -1,12 +1,11 @@
-// strings1.c
 #include <stdio.h>
-#define MSG "I am a symbolic string constant"
+#define MSG "I am a symbolic string constant."
 #define MAXLENGTH 81
 int main(void) 
 {
-    char words[MAXLENGTH] = "I am a symbolic string constant";
+    char words[MAXLENGTH] = "I am a string in an array.";
     const char * pt1 = "Something is pointing at me.";
-    puts("Here are some strings:");
+    puts("Here are some strings");
     puts(MSG);
     puts(words);
     puts(pt1);
