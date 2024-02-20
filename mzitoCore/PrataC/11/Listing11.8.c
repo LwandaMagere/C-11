495c00bc1e6b8901a84a396ec945a8bbf3ff604a
LwandaMagere <dadolenssen@gmail.com>
2024-02-20 14:54:43 +0300

    r

--- mzitoCore/PrataC/11/Listing11.8.c
+++ mzitoCore/PrataC/11/Listing11.8.c
@@ -1,14 +1,15 @@
-/* fgets2.c -- using fgets() and fputs() */
+/* using fgets() and fputs() */
 #include <stdio.h>
 #define STLEN 10
-int main(void) 
+int main(void)
 {
     char words[STLEN];
 
     puts("Enter strings (empty line to quit):");
-    while (fgets(words, STLEN, stdin))
+    while (fgets(words, STLEN, stdin) != NULL && words[0] != '\n')
         fputs(words, stdout);
-    puts("Done");
+    puts("Done.");
     
     return 0;
 }
+
