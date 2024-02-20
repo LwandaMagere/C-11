d4d16df9e8e22622087d2200de03f7eeeaec5b33
LwandaMagere <dadolenssen@gmail.com>
2024-02-20 14:42:23 +0300

    r

--- mzitoCore/PrataC/11/Listing11.7.c
+++ mzitoCore/PrataC/11/Listing11.7.c
@@ -1,4 +1,5 @@
-/* fgets1.c -- using fgets() and fputs() */
+/*  using fgets() and fputs() */
+
 #include <stdio.h>
 #define STLEN 14
 int main(void) 
@@ -9,7 +10,7 @@ int main(void)
     fgets(words, STLEN, stdin);
     printf("Your string twice (puts(), then fputs()):\n");
     puts(words);
-    fputs(words, stdout);
+    fputs(words,stdout);
     puts("Enter another string, please.");
     fgets(words, STLEN, stdin);
     printf("Your string twice (puts(), then fputs()):\n");
