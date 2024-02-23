00247d64c187c3a8b5840f1d405e7250a3eee87e
LwandaMagere <dadolenssen@gmail.com>
2024-02-23 10:06:32 +0300

    r

--- mzitoCore/PrataC/11/Listing11.9.c
+++ mzitoCore/PrataC/11/Listing11.9.c
@@ -1,4 +1,4 @@
-/* fgets3.c -- using fgets() */
+/* using fgets() */
 #include <stdio.h>
 #define STLEN 10
 int main(void) 
@@ -7,20 +7,20 @@ int main(void)
     int i;
 
     puts("Enter strings (empty line to quit):");
-    while (fgets(words, STLEN, stdin) != NULL && words[0] != '\n')
+    while (fgets(words, STLEN, stdin) != NULL && words[0] != '\n') 
     {
         i = 0;
         while (words[i] != '\n' && words[i] != '\0')
-            ++i;
+            i++;
         if (words[i] == '\n')
             words[i] = '\0';
         else // must have words[i] == '\0'
             while (getchar() != '\n')
                 continue;
         puts(words);
-            
     }
+    puts("Done.");
 
-    puts(words);
+
     return 0;
 }
