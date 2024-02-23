d66c9b76a28fbf556333b324271ffc3b54576883
LwandaMagere <dadolenssen@gmail.com>
2024-02-23 10:15:43 +0300

    r

--- mzitoCore/PrataC/11/Listing11.10.c
+++ mzitoCore/PrataC/11/Listing11.10.c
@@ -4,15 +4,16 @@ char * s_gets(char * st, int n)
     int i = 0;
 
     ret_val = fgets(st, n, stdin);
-    if (ret_val) 
+    if (ret_val) // i.e if ret_val != NULL
     {
         while (st[i] != '\n' && st[i] != '\0')
-            ++i;
+            i++;
         if (st[i] == '\n')
             st[i] = '\0';
         else // must have words[i] == '\0'
-        while (getchar() != '\n')
-            continue;
+            while (getchar() != '\n')
+                continue;
+
     }
     return ret_val;
 }
