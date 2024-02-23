80ea80e30ba53c39828d2a6f50fc89c0ecf4219e
LwandaMagere <dadolenssen@gmail.com>
2024-02-23 10:26:45 +0300

    r

--- mzitoCore/PrataC/11/Listing11.10.c
+++ mzitoCore/PrataC/11/Listing11.10.c
@@ -19,3 +19,8 @@ char * s_gets(char * st, int n)
     }
     return ret_val;
 }
+
+/*If fgets() returns NULL , indicating end-of-file or a read error, s_gets() skips the rest of the
+processing. Otherwise, it imitates Listing 11.9 , replacing the newline character with a null char-
+acter if the former is present in the string, and discarding the rest of the line otherwise. It then
+returns the same value fgets() returned.*/
