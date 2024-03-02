f3870f50fefa8f4a74004510f6bd1512e259c2d8
LwandaMagere <dadolenssen@gmail.com>
2024-03-02 20:30:28 +0300

    r

---
+++ mzitoCore/PrataC/12/Listing12.11.c
@@ -0,0 +1,37 @@
+/* diceroll.c -- dice role simulation */
+/* compile with mandydice.c */
+//#include "diceroll.h"
+#include <stdio.h>
+#include <stdlib.h>  /* for library rand() */
+
+int roll_count  = 0;
+
+static int rollem(int sides) 
+{
+    int roll;
+
+    roll = rand() % sides + 1;
+    ++roll_count;
+
+    return roll;
+}
+
+int roll_n_dice(int dice, int sides) 
+{
+    int d;
+    int total = 0;
+    
+    if (sides < 2) 
+    {
+        printf("Need at least 2 sides.\n");
+        return -2;
+    }
+
+    if (dice < 1)
+    {
+        printf("Need at least 1 die.\n");
+        return -1;
+    }
+
+    return total;
+}
