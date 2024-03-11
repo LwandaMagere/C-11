950ba7cb6ddcd6ebd778f21ee43765724b732f23
LwandaMagere <dadolenssen@gmail.com>
2024-03-11 07:18:12 +0300

    r

--- mzitoCore/PrataC/3/Listing3.4.c
+++ mzitoCore/PrataC/3/Listing3.4.c
@@ -1,9 +1,9 @@
+/* print2.c-more printf() properties */
 #include <stdio.h>
-
-int main(void)
+int main(void) 
 {
-    unsigned int un = 3000000000;
-    short end = 200;
+    unsigned int un = 3000000000; /* system with 32-bit int */
+    short end = 200; /* and 16-bit short*/
     long big = 65537;
     long long verybig = 12345678908642;
 
@@ -11,6 +11,6 @@ int main(void)
     printf("end = %hd and %d\n", end, end);
     printf("big = %ld and not %hd\n", big, big);
     printf("verybig= %lld and not %ld\n", verybig, verybig);
-
+    
     return 0;
 }
