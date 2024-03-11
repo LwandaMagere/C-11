a0d3c9282e7a1622d1a16463b0955feb84c1e77c
LwandaMagere <dadolenssen@gmail.com>
2024-03-11 16:05:31 +0300

    r

--- mzitoCore/PrataC/5/Listing5.1.c
+++ mzitoCore/PrataC/5/Listing5.1.c
@@ -1,14 +1,15 @@
+/* shoes1.c -- converts a shoe size to inches */
 #include <stdio.h>
 #define ADJUST 7.31
-
-int main()
+int main(void) 
 {
     const double SCALE = 0.333;
-    double shoes, foot;
+    double shoe, foot;
 
-    shoes = 9.0;
-    foot = SCALE * shoes + ADJUST;
+    shoe = 9.0;
+    foot = SCALE * shoe + ADJUST;
     printf("Shoe size (men's) foot length\n");
+    printf("%10.1f %15.2f inches\n", shoe, foot);
     
     return 0;
 }
