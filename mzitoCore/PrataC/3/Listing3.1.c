18f5f90c07b8e12b3ba7410a4a6d37269a9913d5
LwandaMagere <dadolenssen@gmail.com>
2024-03-11 06:33:34 +0300

    r

--- mzitoCore/PrataC/3/Listing3.1.c
+++ mzitoCore/PrataC/3/Listing3.1.c
@@ -1,5 +1,5 @@
+/* platinum.c -- your weight in platinum */
 #include <stdio.h>
-
 int main(void) 
 {
     float weight;
@@ -9,12 +9,14 @@ int main(void)
     printf("Let's check it out.\n");
     printf("Please enter your weight in pounds: ");
 
-    // get input from user
-    scanf("%f", & weight);
+    // get input from the user
+    scanf("%f", &weight);
+    /* assume platinum is $1700 per ounce */
+    /* 14.5833 converts pounds avd. to ounces troy */
     value = 1700.0 * weight * 14.5833;
     printf("Your weight in platinum is worth $%.2f.\n", value);
     printf("You are easily worth that! If platinum prices drop,\n");
     printf("eat more to maintain your value.\n");
-    
+
     return 0;
 }
