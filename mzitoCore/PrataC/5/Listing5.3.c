ba51b1a83a2dc9bcbb47642d6aa4b80d87697ec5
LwandaMagere <dadolenssen@gmail.com>
2024-03-11 16:19:49 +0300

    r

--- mzitoCore/PrataC/5/Listing5.3.c
+++ mzitoCore/PrataC/5/Listing5.3.c
@@ -1,12 +1,11 @@
+/* golf.c -- golf tournament scorecard */
 #include <stdio.h>
-
-int main()
+int main(void) 
 {
     int jane, tarzan, cheeta;
 
     cheeta = tarzan = jane = 68;
-    printf("                cheeta tarzan jane\n");
+    printf(" cheeta tarzan jane\n");
     printf("First round score %4d %8d %8d\n",cheeta,tarzan,jane);
-    
     return 0;
 }
