155042af204b2d360bbf43a817881bfa7be3bf8b
LwandaMagere <dadolenssen@gmail.com>
2024-03-11 13:02:06 +0300

    r

--- mzitoCore/PrataC/4/Listing4.2.c
+++ mzitoCore/PrataC/4/Listing4.2.c
@@ -1,5 +1,6 @@
+/* praise1.c -- uses an assortment of strings */
 #include <stdio.h>
-#define PRAISE "You are an extraodinary being."
+#define PRAISE "You are an extraordinary being."
 int main(void) 
 {
     char name[40];
