1fc4c9bf3e336d5e1117ffd715bd3c9e2854ebcc
LwandaMagere <dadolenssen@gmail.com>
2024-03-11 12:54:26 +0300

    r

--- mzitoCore/PrataC/4/Listing4.1.c
+++ mzitoCore/PrataC/4/Listing4.1.c
@@ -11,11 +11,11 @@ int main(void)
     printf("Hi! What's your first name?\n");
     scanf("%s", name);
     printf("%s, what's your weight in pounds?\n", name);
-    scanf("%d", &weight);
+    scanf("%f", &weight);
     size = sizeof name;
     letters = strlen(name);
     volume = weight / DENSITY;
-    printf("Well, %s, your volume is %2.2f cubic feet.\n", name, volume);
+    printf("Well, %s, your volume is %3.2f cubic feet.\n", name, volume);
     printf("Also, your first name has %d letters,\n", letters);
     printf("and we have %d bytes to store it.\n", size);
 
