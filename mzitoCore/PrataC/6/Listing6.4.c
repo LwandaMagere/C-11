1c4df5a1b1b2e5091678ea49f25360c02d52622a
LwandaMagere <dadolenssen@gmail.com>
2024-03-12 10:50:51 +0300

    r

--- mzitoCore/PrataC/6/Listing6.4.c
+++ mzitoCore/PrataC/6/Listing6.4.c
@@ -3,9 +3,10 @@
 int main(void)
 {
     int n = 0;
-    while (n++ < 3); /* line 7 */
+
+    while (n++ < 3);  /* line 7 */
         printf("n is %d\n", n); /* line 8 */
     printf("That's all this program does.\n");
-
+    
     return 0;
 }
