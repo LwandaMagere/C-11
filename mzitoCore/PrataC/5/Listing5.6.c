f49edea348e711df4a17ad2b3e0aefb30d120717
LwandaMagere <dadolenssen@gmail.com>
2024-03-12 09:28:44 +0300

    r

--- mzitoCore/PrataC/5/Listing5.6.c
+++ mzitoCore/PrataC/5/Listing5.6.c
@@ -1,6 +1,6 @@
 /* divide.c -- divisions we have known */
 #include <stdio.h>
-int main(void)
+int main(void) 
 {
     printf("integer division: 5/4 is %d \n", 5/4);
     printf("integer division: 6/3 is %d \n", 6/3);
