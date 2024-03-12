c4f74d2076bb28653acf1dffe411884b67247363
LwandaMagere <dadolenssen@gmail.com>
2024-03-12 10:23:31 +0300

    r

--- mzitoCore/PrataC/5/Listing5.15.c
+++ mzitoCore/PrataC/5/Listing5.15.c
@@ -1,20 +1,22 @@
+/* pound.c -- defines a function with an argument */
 #include <stdio.h>
-void pound(int);
-
-int main(void)
+void pound(int n); // ANSI function prototype declaration
+int main(void) 
 {
     int times = 5;
     char ch = '!'; // ASCII code is 33
     float f = 6.0f;
+
     pound(times);
     pound(ch);
     pound(f);
+    
     return 0;
 }
 
-void pound(int n) 
+void pound(int n)
 {
     while (n-- > 0)
-        printf("#");
+    printf("#");
     printf("\n");
 }
