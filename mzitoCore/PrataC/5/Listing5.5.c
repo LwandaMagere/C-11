7c194d928201b695c7641dbe200830e688520c8c
LwandaMagere <dadolenssen@gmail.com>
2024-03-12 09:25:15 +0300

    r

--- mzitoCore/PrataC/5/Listing5.5.c
+++ mzitoCore/PrataC/5/Listing5.5.c
@@ -1,27 +1,27 @@
+/* wheat.c -- exponential growth */
 #include <stdio.h>
-#define SQUARES 64
-
+#define SQUARE 64
 int main(void) 
 {
-    const double CROP = 2E16;
+    const double CROP = 2E6;
     double current, total;
-    int count =1;
-    printf("square  grains  total ");
-    printf("fraction of \n");
-    printf("    added   grains  ");
-    printf("world total\n");
-    total = current = 1.0; 
-    printf("%4d %13.2e %12.2e %12.2e", count, current, total, total/CROP);
+    int count = 1;
 
-    while (count < SQUARES)
+    printf("square  grains  total");
+    printf("fraction of \n");
+    printf("added   grains  ");
+    printf("world total\n");
+    total = current = 1.0; /* start with one grain  */
+    printf("%4d %13.2e %12.2e %12.2e\n", count, current, total, total/CROP);
+    while (count < SQUARE)
     {
         count = count + 1;
-        current = 2.0 * current; /* double grains on next square */
-        total = total + current;/* update total */
-        printf("%4d %13.2e %12.2e %12.2e\n", count, current,
-        total, total/CROP);
+        current = 2.0 * current;
+        /* double grains on next square */
+        total = total + current; /* update total */
+        printf("%4d %13.2e %12.2e %12.2e\n",count, current,total, total/CROP);
     }
     printf("That's all.\n");
-
+    
     return 0;
 }
