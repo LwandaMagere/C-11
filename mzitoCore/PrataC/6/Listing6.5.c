962e44bb59cbc952db673abe8c5011e4fbd1652c
LwandaMagere <dadolenssen@gmail.com>
2024-03-12 10:57:01 +0300

    r

--- mzitoCore/PrataC/6/Listing6.5.c
+++ mzitoCore/PrataC/6/Listing6.5.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+// cmpflt.c -- floating-point comparisons
 #include <math.h>
-
+#include <stdio.h>
 int main(void) 
 {
     const double ANSWER = 3.14159;
@@ -8,13 +8,13 @@ int main(void)
 
     printf("What is the value of pi?\n");
     scanf("%lf", &response);
-    while (fabs(response - ANSWER) > 0.0001)
+
+    while (fabs(response - ANSWER) > 0.0001) 
     {
         printf("Try again!\n");
         scanf("%lf", &response);
     }
     printf("Close enough!\n");
     
-
     return 0;
 }
