0652c27dc87e3669302c4c1e764917cc2efcf26e
LwandaMagere <dadolenssen@gmail.com>
2024-03-14 11:43:58 +0300

    r

---
+++ mzitoCore/PrataC++/3/Listing3.10.cxx
@@ -0,0 +1,17 @@
+// arith.cpp -- some C++ arithmetic
+#include <iostream>
+
+int main() 
+{
+    using namespace std;
+    float hats, heads;
+
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+    cout << "Enter a number: ";
+    cin >> hats;
+    cout << "Enter another number: ";
+    cin >> heads;
+
+    
+    return 0;
+}
