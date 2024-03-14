f5e0aa3dc1cf2377e3bb092ba81e87317bfb5379
LwandaMagere <dadolenssen@gmail.com>
2024-03-14 11:01:05 +0300

    r

---
+++ mzitoCore/PrataC++/2/Listing2.6.cxx
@@ -0,0 +1,22 @@
+// convert.cpp -- converts stone to pounds
+
+#include <iostream>
+int stonetolb(int);
+
+int main()
+{
+    using namespace std;
+    int stone;
+    cout << "Enter the weight in stone: ";
+    cin >> stone;
+    int pounds = stonetolb(stone);
+    cout << stone << " stone = ";
+    cout << pounds << " pounds." << endl;
+
+    return 0;
+}
+
+int stonetolb(int a) 
+{
+    return 14 * a;
+}
