23d855d50cd39a632d58178c6391cefafad13c3c
LwandaMagere <dadolenssen@gmail.com>
2024-03-14 10:15:20 +0300

    r

---
+++ mzitoCore/PrataC++/2/Listing2.1.cxx
@@ -0,0 +1,12 @@
+// myfirst.cpp -- displays a message
+
+#include <iostream>     // a PREPROCESSOR directive
+int main()
+{
+    using namespace std;   // make definitions visible
+    cout << "Come up and C++ me sometime";
+    cout << endl;
+    cout << "You won’t regret it!" << endl;
+
+    return 0;    // terminate main()
+}               // end of function body
