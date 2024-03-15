131aaa835f5c81b2db56d7784d00f79d84b9b3af
LwandaMagere <dadolenssen@gmail.com>
2024-03-15 11:09:11 +0300

    r

--- mzitoCore/PrataC/6/Listing6.8.c
+++ mzitoCore/PrataC/6/Listing6.8.c
@@ -1,20 +1,23 @@
+// trouble.c -- misuse of =
+// will cause infinite loop
 #include <stdio.h>
-int main(void)
+int main(void) 
 {
     long num;
     long sum = 0L;
     int status;
-    
     printf("Please enter an integer to be summed ");
     printf("(q to quit): ");
+
     status = scanf("%ld", &num);
-    while (status = 1)
+    while (status = 1) 
     {
-        sum = sum + num;
+        sum = sum + 1;
         printf("Please enter next integer (q to quit): ");
         status = scanf("%ld", &num);
+        
     }
     printf("Those integers sum to %ld.\n", sum);
-
+    
     return 0;
 }
