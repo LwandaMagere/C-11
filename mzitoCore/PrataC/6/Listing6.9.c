b7b0033e28b56a7c698a6372f86c2f8e8a3be2d1
LwandaMagere <dadolenssen@gmail.com>
2024-03-15 11:19:48 +0300

    r

--- mzitoCore/PrataC/6/Listing6.9.c
+++ mzitoCore/PrataC/6/Listing6.9.c
@@ -1,21 +1,26 @@
+// boolean.c -- using a _Bool variable
 #include <stdio.h>
-
 int main(void) 
 {
-    long num;
-    long sum = 0L;
-    _Bool input_is_good;
+    // long num;
+    // long sum = 0L;
+    // _Bool input_is_good;
 
-    printf("Please enter an integer to be summed ");
-    printf("(q to quit): ");
-    input_is_good = (scanf("%ld", &num) == 1);
-    while (input_is_good)
-    {
-        sum = sum + num;
-        printf("Please enter next integer (q to quit): ");
-        input_is_good = (scanf("%ld", &num) == 1);
-    }
-    printf("Those integers sum to %ld.\n", sum);
+    // printf("Please enter an integer to be summed ");
+    // printf("(q to quit): ");
+    // input_is_good = (scanf("%ld", &num) == 1);
+    // while (input_is_good) 
+    // {
+    //     sum = sum + num;
+    //     printf("Please enter next integer (q to quit): ");
+    //     input_is_good = (scanf("%ld", &num) == 1);
+    // }
+    // printf("Those integers sum to %ld.\n", sum);
+
+    int Steve, Dado;
+    printf("Write somethi\n");
+    int sum = scanf("%d %d", &Steve, &Dado);
+    printf("This is it %d", sum);
 
     return 0;
 }
