826d7496596a73359defc8ffc86e64a16a35dce8
LwandaMagere <dadolenssen@gmail.com>
2024-03-15 13:40:51 +0300

    r

--- mzitoCore/PrataC/6/Listing6.20.c
+++ mzitoCore/PrataC/6/Listing6.20.c
@@ -1,7 +1,7 @@
 // power.c -- raises numbers to integer powers
 #include <stdio.h>
 double power(double n, int p); // ANSI prototype
-int main(void)
+int main(void) 
 {
     double x, xpow;
     int exp;
@@ -9,21 +9,23 @@ int main(void)
     printf("Enter a number and the positive integer power");
     printf(" to which\nthe number will be raised. Enter q");
     printf(" to quit.\n");
-    while (scanf("%lf%d", &x, &exp) == 2)
+    while (scanf("%lf%d", &x, &exp)) 
     {
-        xpow = power(x,exp); // function call
+        xpow = power(x, exp);
         printf("%.3g to the power %d is %.5g\n", x, exp, xpow);
         printf("Enter next pair of numbers or q to quit.\n");
-    }
+    } 
     printf("Hope you enjoyed this power trip -- bye!\n");
+
 
     return 0;
 }
-double power(double n, int p)
+
+double power(double n, int p)   // function definition
 {
     double pow = 1;
-    int i;    // function definition
+    int i;
     for (i = 1; i <= p; i++)
-    pow *= n;
-    return pow; // return the value of pow
+        pow *= n;
+    return pow;   // return the value of pow
 }
