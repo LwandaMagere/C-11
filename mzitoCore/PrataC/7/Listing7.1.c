c29801f36b4aef171be182482e403c6860f39e4f
LwandaMagere <dadolenssen@gmail.com>
2024-03-15 13:55:17 +0300

    r

--- mzitoCore/PrataC/7/Listing7.1.c
+++ mzitoCore/PrataC/7/Listing7.1.c
@@ -1,6 +1,6 @@
 // colddays.c -- finds percentage of days below freezing
 #include <stdio.h>
-int main(void)
+int main(void) 
 {
     const int FREEZING = 0;
     float temperature;
@@ -16,9 +16,10 @@ int main(void)
             cold_days++;
     }
     if (all_days != 0)
-        printf("%d days total: %.1f%% were below freezing.\n", all_days, 100.0 * (float) cold_days /all_days);
+    printf("%d days total: %.1f%% were below freezing.\n",
+            all_days, 100.0 * (float) cold_days / all_days);
     if (all_days == 0)
         printf("No data entered!\n");
-
+        
     return 0;
 }
