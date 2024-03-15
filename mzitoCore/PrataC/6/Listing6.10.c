bad71ad4261ddd35eb106ffd21a9c1619b4d5524
LwandaMagere <dadolenssen@gmail.com>
2024-03-15 12:45:59 +0300

    r

--- mzitoCore/PrataC/6/Listing6.10.c
+++ mzitoCore/PrataC/6/Listing6.10.c
@@ -1,15 +1,14 @@
+// sweetie1.c -- a counting loop
 #include <stdio.h>
-
-int main(void)
+int main(void) 
 {
     const int NUMBER = 22;
     int count = 1;
-    
-    while (count <= NUMBER)
+
+    while (count <= NUMBER) 
     {
         printf("Be my Valentine!\n");
         count++;
     }
-
     return 0;
 }
