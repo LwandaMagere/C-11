38c1b24f0ebba9ce68d1240a3b0b8e16c431f867
LwandaMagere <dadolenssen@gmail.com>
2024-03-18 17:40:20 +0300

    r

--- mzitoCore/PrataC/7/Listing7.7.c
+++ mzitoCore/PrataC/7/Listing7.7.c
@@ -1,12 +1,12 @@
 // wordcnt.c -- counts characters, words, lines
 #include <stdio.h>
 #include <ctype.h> // for isspace()
-#include <stdbool.h> // for bool, true, false
+#include <stdbool.h>  // for bool, true, false
 #define STOP '|'
 
-int main(void)
+int main(void) 
 {
-    char c; // read in character
+    char c;        // read in character
     char prev; // previous character read
     long n_chars = 0L; // number of characters
     int n_lines = 0; // number of lines
@@ -15,20 +15,20 @@ int main(void)
     bool inword = false; // == true if c is in a word
 
     printf("Enter text to be analyzed (| to terminate):\n");
-    prev = '\n'; // used to identify complete lines
+    prev = '\n';  // used to identify complete lines
     while ((c = getchar()) != STOP)
     {
-        n_chars++; // count characters
+        n_chars++;  // count characters
         if (c == '\n')
-            n_lines++; // count lines
+            n_lines++;   // count lines
         if (!isspace(c) && !inword)
         {
             inword = true; // starting a new word
-            n_words++; // count word
+            n_words++;   // count word
         }
         if (isspace(c) && inword)
             inword = false; // reached end of word
-        prev = c; // save character value
+        prev = c;     // save character value
     }
     if (prev != '\n')
         p_lines = 1;
