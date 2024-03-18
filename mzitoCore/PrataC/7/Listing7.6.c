327d0c393ddcff253012d2730d576621b9a5f7d7
LwandaMagere <dadolenssen@gmail.com>
2024-03-18 17:33:48 +0300

    r

--- mzitoCore/PrataC/7/Listing7.6.c
+++ mzitoCore/PrataC/7/Listing7.6.c
@@ -1,18 +1,16 @@
 // chcount.c -- use the logical AND operator
 #include <stdio.h>
 #define PERIOD '.'
-
-int main(void)
+int main()
 {
     char ch;
     int charcount = 0;
-
-    while ((ch = getchar()) != PERIOD)
+    while ((ch = getchar()) != PERIOD) 
     {
         if (ch != '"' && ch != '\'')
             charcount++;
     }
     printf("There are %d non-quote characters.\n", charcount);
-
+    
     return 0;
 }
