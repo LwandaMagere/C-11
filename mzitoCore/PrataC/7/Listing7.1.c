b19fc1ef3b058f9d787aa5a43d4586ddbc9a0586
LwandaMagere <dadolenssen@gmail.com>
2024-03-18 17:12:26 +0300

    r

--- mzitoCore/PrataC/7/Listing7.1.c
+++ mzitoCore/PrataC/7/Listing7.1.c
@@ -1,25 +1,21 @@
 // colddays.c -- finds percentage of days below freezing
 #include <stdio.h>
-int main(void) 
-{
-    const int FREEZING = 0;
-    float temperature;
-    int cold_days = 0;
-    int all_days = 0;
+#define SPACE ' '
 
-    printf("Enter the list of daily low temperatures.\n");
-    printf("Use Celsius, and enter q to quit.\n");
-    while (scanf("%f", &temperature) == 1)
+int main() 
+{
+    char ch;
+
+    ch = getchar();   // read a character
+    while (ch != '\n') // while not end of line 
     {
-        all_days++;
-        if (temperature < FREEZING)
-            cold_days++;
+        if (ch == SPACE)
+            putchar(ch);
+        else 
+            putchar(ch + 1);
+        ch = getchar();
     }
-    if (all_days != 0)
-    printf("%d days total: %.1f%% were below freezing.\n",
-            all_days, 100.0 * (float) cold_days / all_days);
-    if (all_days == 0)
-        printf("No data entered!\n");
-        
+    putchar(ch);
+
     return 0;
 }
