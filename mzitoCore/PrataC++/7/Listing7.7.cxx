45a1fc046c3bc6d1ea108709db12d2c2c4d3c07f
LwandaMagere <dadolenssen@gmail.com>
2024-03-19 16:29:25 +0300

    r

--- mzitoCore/PrataC++/7/Listing7.7.cxx
+++ mzitoCore/PrataC++/7/Listing7.7.cxx
@@ -32,3 +32,46 @@ int main()
     cin.get();
     return 0;
 }
+
+int fill_array(double ar[], int limit)
+{
+    using namespace std;
+    double temp;
+    int i;
+    for (i = 0; i < limit; i++)
+    {
+    cout << "Enter value #" << (i + 1) << ": ";
+    cin >> temp;
+    if (!cin)   // bad input
+    {
+        cin.clear();
+        while (cin.get() != '\n')
+            continue;
+        cout << "Bad input; input process terminated.\n";
+        break;
+    }
+    else if (temp < 0)
+        break;
+    ar[i] = temp;   // signal to terminate
+    }
+    return i;
+}
+
+// the following function can use, but not alter,
+// the array whose address is ar
+void show_array(const double ar[], int n)
+{
+    using namespace std;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Property #" << (i + 1) << ": $";
+        cout << ar[i] << endl;
+    }
+}
+
+// multiplies each element of ar[] by r
+void revalue(double r, double ar[], int n)
+{
+    for (int i = 0; i < n; i++)
+        ar[i] *= r;
+}
