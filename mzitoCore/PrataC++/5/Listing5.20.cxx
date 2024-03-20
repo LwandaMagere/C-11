f96a9d26a50732a84ca7029b5d1965876a658426
LwandaMagere <dadolenssen@gmail.com>
2024-03-20 10:01:56 +0300

    r

--- mzitoCore/PrataC++/5/Listing5.20.cxx
+++ mzitoCore/PrataC++/5/Listing5.20.cxx
@@ -28,7 +28,7 @@ int main()
     cout << "Maximum temperatures for 2008 - 2011\n\n";
     for (int city = 0; city < Cities; ++city)
     {
-        cout << cities[city] << ":\t";
+        cout << (*cities)[city] << ":\t";
         for (int year = 0; year < Years; ++year)
             cout << maxtemps[year][city] << "\t";
         cout << endl;
