a7ceea81157b5328eac0eb01542f298580bd273d
LwandaMagere <dadolenssen@gmail.com>
2024-03-26 16:25:42 +0300

    r

---
+++ mzitoCore/PrataC++/10/Listing10.1.h
@@ -0,0 +1,24 @@
+// stock00.h -- Stock class interface
+// version 00
+#ifndef STOCK00_H_
+#define  STOCK00_H_
+
+#include <string>
+
+class Stock
+{
+    private:
+        std::string company;
+        long shares;
+        double share_val;
+        double total_val;
+        void set_tot() {total_val = shares * share_val;}
+    public:
+        void acquire(const std::string &, long, double);
+        void buy(long, double);
+        void sell(long, double);
+        void update(double price);
+        void show();
+};  // note semicolon at the end
+
+#endif
