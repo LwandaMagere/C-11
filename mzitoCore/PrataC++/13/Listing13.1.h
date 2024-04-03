5b80d6b15a9fe5f23c55ba06a31c338f6af1ca1a
LwandaMagere <dadolenssen@gmail.com>
2024-04-03 08:05:39 +0300

    r

---
+++ mzitoCore/PrataC++/13/Listing13.1.h
@@ -0,0 +1,20 @@
+#ifndef TABTENN0_H_
+#define TABTENN0_H_
+
+#include <string>
+
+using std::string;
+class TableTennisPlayer
+{
+    private:
+        string firstname;
+        string lastname;
+        bool hasTable;
+    public:
+        TableTennisPlayer(const string & fn = "none", const string & ls = "none", bool hasTable = false);
+        void Name() const;
+        bool HasTable() const { return hasTable;}
+        void ResetTable(bool v) {hasTable = v;}
+};
+
+#endif
