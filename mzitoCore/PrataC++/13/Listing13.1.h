e951a07814539c2e766cd371a9b7aff6b99c2bf7
LwandaMagere <dadolenssen@gmail.com>
2024-04-13 19:31:08 +0300

    r

--- mzitoCore/PrataC++/13/Listing13.1.h
+++ mzitoCore/PrataC++/13/Listing13.1.h
@@ -1,20 +1,19 @@
-#ifndef TABTENN0_H_
-#define TABTENN0_H_
-
-#include <string>
-
+// tabtenn0.h -- a table-tennis base class
+#ifndef TABTENNO_H_
+#define TABTENNO_H_
 using std::string;
-class TableTennisPlayer
+
+class TableTennisPlayer 
 {
     private:
         string firstname;
         string lastname;
         bool hasTable;
     public:
-        TableTennisPlayer(const string & fn = "none", const string & ls = "none", bool hasTable = false);
+        TableTennisPlayer(const string & fn = "none", const string ln = "none", bool ht = false);
         void Name() const;
-        bool HasTable() const { return hasTable;}
-        void ResetTable(bool v) {hasTable = v;}
+        bool HasTable() const { return hasTable; };
+        void ResetTable(bool v) { hasTable = v; };
+        
 };
-
 #endif
