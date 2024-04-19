e669a501025c6319b324a321aa371c9ff648177f
LwandaMagere <dadolenssen@gmail.com>
2024-04-19 21:25:05 +0300

    r

---
+++ mzitoCore/PrataC++/12/Listing12.1.h
@@ -0,0 +1,19 @@
+// strngbad.h -- flawed string class definition
+#include <iostream>
+#ifndef STRNGBAD_H_
+#define STRNGBAD_H_
+
+class StringBad
+{
+    private:
+        char * str;
+        int len;
+        static int num_string;
+    public:
+        StringBad(const char *);
+        StringBad();
+        ~StringBad();
+        friend std::ostream & operator<<(std::ostream & os, const StringBad &);
+};
+
+#endif
