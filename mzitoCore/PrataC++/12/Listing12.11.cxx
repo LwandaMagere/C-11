07576d4710682b5769971faebb39c7be4acc82e2
LwandaMagere <dadolenssen@gmail.com>
2024-04-24 19:54:37 +0300

    r

--- mzitoCore/PrataC++/12/Listing12.11.cxx
+++ mzitoCore/PrataC++/12/Listing12.11.cxx
@@ -1,80 +1,207 @@
 // queue.cpp -- Queue and Customer methods
 #include "Listing12.10.h"
-#include <cstdlib> // (or stdlib.h) for rand()
-
-Queue::Queue(int qs) : qsize(qs) 
+#include <cstdlib>
+// (or stdlib.h) for rand()
+// Queue methods
+Queue::Queue(int qs) : qsize(qs)
 {
-    front = rear = nullptr;
-    items = 0;
+front = rear = NULL;    // or nullptr
+items = 0;
 }
 
 Queue::~Queue()
 {
-    Node * temp;
-    while (front != nullptr)
-    {
-        temp = front; // save adress of front item;
-        front = front->next; // reset pointer to next item
-        delete temp;
-    }
-    
+Node * temp;
+while (front != NULL)
+//
+{
+temp = front;
+//
+front = front->next;//
+delete temp;
+//
+}
 }
 
 bool Queue::isempty() const
 {
-    return items == 0;
+return items == 0;
 }
 
 bool Queue::isfull() const
 {
-    return items == qsize;
+return items == qsize;
 }
 
 int Queue::queuecount() const
 {
-    return items;
+return items;
 }
 
 // Add item to queue
 bool Queue::enqueue(const Item & item)
 {
-    if (isfull())
-        return false;
-    Node * add = new Node;  // create node
-    add->item = item;
-    add->next = nullptr;
-    items++;
-    if (front = nullptr) // if queue is empty,
-        front = add;  // place item at front
-    else
-        rear->next = add;
-    rear = add;   // have rear point to new node
-    return true;
-
+if (isfull())
+return false;
+Node * add = new Node; // create node
+// on failure, new throws std::bad_alloc exception
+add->item = item;
+// set node pointers
+add->next = NULL;
+// or nullptr;
+items++;
+if (front == NULL)
+// if queue is empty,
+front = add;
+// place item at front
+else
+rear->next = add;
+// else place at rear
+rear = add;
+// have rear point to new node
+return true;
 }
 
 // Place front item into item variable and remove from queue
 bool Queue::dequeue(Item & item)
 {
-    if (front = nullptr)
-        return false;
-    item = front->item;  // set item to first item in queue
-    items--;
-    Node * temp = front;  // save location of first item
-    front = front->next;  // reset front to next item
-    delete temp;     // delete former first item
-    if (items == 0)
-        rear = nullptr;
-    return true;
+if (front == NULL)
+return false;
+item = front->item;
+// set item to first item in queue
+items--;
+Node * temp = front;
+// save location of first item
+front = front->next;
+// reset front to next item
+delete temp;
+// delete former first item
+if (items == 0)
+rear = NULL;
+return true;
 }
 
-// customer method
-
-// when is the time at which the customer arrives
-// the arrival time is set to when and the processing
-// time set to a random value in the range 1 - 3
 void Customer::set(long when)
 {
-    processtime = std::rand() % 3 + 1;
-    arrive = when;
+processtime = std::rand() % 3 + 1;
+arrive = when;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // queue.cpp -- Queue and Customer methods
+// #include "Listing12.10.h"
+// #include <cstdlib> // (or stdlib.h) for rand()
+
+// Queue::Queue(int qs) : qsize(qs) 
+// {
+//     front = rear = nullptr;
+//     items = 0;
+// }
+
+// Queue::~Queue()
+// {
+//     Node * temp;
+//     while (front != nullptr)
+//     {
+//         temp = front; // save adress of front item;
+//         front = front->next; // reset pointer to next item
+//         delete temp;
+//     }
+    
+// }
+
+// bool Queue::isempty() const
+// {
+//     return items == 0;
+// }
+
+// bool Queue::isfull() const
+// {
+//     return items == qsize;
+// }
+
+// int Queue::queuecount() const
+// {
+//     return items;
+// }
+
+// // Add item to queue
+// bool Queue::enqueue(const Item & item)
+// {
+//     if (isfull())
+//         return false;
+//     Node * add = new Node;  // create node
+//     add->item = item;
+//     add->next = nullptr;
+//     items++;
+//     if (front = nullptr) // if queue is empty,
+//         front = add;  // place item at front
+//     else
+//         rear->next = add;
+//     rear = add;   // have rear point to new node
+//     return true;
+
+// }
+
+// // Place front item into item variable and remove from queue
+// bool Queue::dequeue(Item & item)
+// {
+//     if (front = nullptr)
+//         return false;
+//     item = front->item;  // set item to first item in queue
+//     items--;
+//     Node * temp = front;  // save location of first item
+//     front = front->next;  // reset front to next item
+//     delete temp;     // delete former first item
+//     if (items == 0)
+//         rear = nullptr;
+//     return true;
+// }
+
+// // customer method
+
+// // when is the time at which the customer arrives
+// // the arrival time is set to when and the processing
+// // time set to a random value in the range 1 - 3
+// void Customer::set(long when)
+// {
+//     processtime = std::rand() % 3 + 1;
+//     arrive = when;
+// }
