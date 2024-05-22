b8776b1c50f5560b79dcbe3f39383627595841ee
LwandaMagere <dadolenssen@gmail.com>
2024-05-22 09:57:53 +0300

    r

--- mzitoCore/PrataC++/11/Listing11.1.h
+++ mzitoCore/PrataC++/11/Listing11.1.h
@@ -13,9 +13,29 @@ class Time
         void AddMin(int m);
         void AddHr(int h);
         void Reset(int h = 0, int m = 0);
-        Time Sum(const Time & t) const;
+        Time sum(const Time & t) const;
         void Show() const;
-        
 };
 
 #endif
+
+// #ifndef MYTIME0_H_
+// #define MYTIME0_H_
+
+// class Time 
+// {
+//     private:
+//         int hours;
+//         int minutes;
+//     public:
+//         Time();
+//         Time(int h, int m = 0);
+//         void AddMin(int m);
+//         void AddHr(int h);
+//         void Reset(int h = 0, int m = 0);
+//         Time Sum(const Time & t) const;
+//         void Show() const;
+        
+// };
+
+// #endif
