14eeed0ed98c852e1ccd86aef773a82e59352b71
LwandaMagere <dadolenssen@gmail.com>
2024-07-22 04:28:10 +0000

    r

--- mzitoJS/JS/01_DataTypeOP/app.js
+++ mzitoJS/JS/01_DataTypeOP/app.js
@@ -1,149 +1,155 @@
                                         // INTRODUCTION
+// Log to console
 
+// console.log("Hi");
+// console.log(true);
+// console.log([1,2,3]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
 
-// **%%$$@@&& key value pair **%%$$@@&&
+// console.error('This is an error');
+// console.clear();
+// console.warn('This is a warning!');
+
+// console.time('Hello');
+//     console.log("Hi");
+//     console.log("Hi");
+//     console.log("Hi");
+//     console.log("Hi");
+//     console.log("Hi");
+//     console.log("Hi");
+// console.timeEnd('Hello');
 
 
 // ********** 1. DATA TYPES ***************************
 
     // 1. Primitives
 
-    // 1// String ......................................
+// String
+// const name = 'Steven Dado';
+// console.log(typeof name);
 
-    // const name = 'Dado Steven';
+// // Number
+// const age = 32;
+// console.log(typeof age);
 
-    // 2// Number
+// // Boolean
+// const hasKids = false;
+// console.log(typeof hasKids);
 
-    // const age = 32;
+// // Null
+// const car = null;
+// console.log(typeof car);
 
-    // 3// Boolean ......................................
+// // Undefined
+// let test;
+// console.log(typeof test);
 
-    // const hasKids = false;
+// // Symbol
+// const sym = Symbol();
+// console.log(typeof sym);
 
-    // 4// Null ............................................
+//      // 2. REFERENCE TYPES - Objects
 
-    // const car = null;
+// //Arrays
+// const hobbies = ['movies', 'music'];
+// console.log(typeof hobbies);
 
-    // 5// Undefined ......................................
+// // Object literal
+// const address = {
+//     city: 'Boston',
+//     state: 'MA'
+// }
+// console.log(typeof address);
 
-    // let test;
+// // Date
+// const today = new Date();
+// console.log(typeof today);
 
-    // 6// Symbol ........................................
+//                         // TYPE CONVERSION
+// let val;
 
-    // const sym = Symbol();
+// // Number to String
+// val = String(5);
+// val = String(4+4);
 
-    // // 2. Reference Type - Objects
+// // Bool to String
+// val = String(true);
+// val = String(new Date());
 
-    // 1// Arrays ................................................
+// // Array to string
+// val = String([1,2,3,4]);
 
-    // const hobbies = ['movies', 'music'];
+// // toString()
+// val = (5).toString();
+// val = (true).toString();
 
-    // 2// Object literal
+// // String to Numbers
+// val = Number('5');
+// val = Number(true);
+// val = Number(null);
+// val = Number('Hello');
+// val = Number([1,2,3]);
 
-    // const address = {
-    //     city: 'Nairobi',
-    //     State: 'Kenya'
-    // };
+// val = parseInt('100');
+// val = parseFloat('100.30');
 
-    // 2// Dates...................................................
+// // output
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length);
+// console.log(val.toFixed());
 
-    // const today = new Date();
-    // console.log(typeof today);
+// Type coursion is done by Javascript
 
-    
-    // ********** 2. TYPE CONVERSION ***************************
+            // STRINGS
 
-        // let val;
+const firstName = 'Dado';
+const lastName = 'Steven';
+const str = 'Hello there my name is Steven';
 
-        // // Number to String ............................
-        // val = String(5);
-        // val = String(4 + 5);
+let val;
 
-        // // Bool to String .....................................
-        // val = String(true);
+// Concatenation
+val = firstName + ' ' + lastName;
+console.log(val);
 
-        // // Date to String ...................................
-        // val = String(new Date());
+// Append
+val = 'Lwanda ';
+val += 'Magere';
+console.log(val);
 
-        // // Array to String
-        // val = String([1,2,3,4]);
+// Length
+val = firstName.length;
+console.log(val);
 
-        // // toString()
-        // val = (44).toString();
-        // val = (true).toString();
+// Concat
+val = firstName.concat(' ', lastName);
+console.log(val);
 
-        // // String to Numbers ....................................
-        // val = Number('4');
-        // val = Number(true);
-        // val = Number(null);
-        // val = Number('Hello');
-        // val = Number([1,2,3,4]);
+// Change case
+val = firstName.toUpperCase();
+console.log(val);
 
-        // val = parseInt('100');
-        // val = parseFloat('100.30');
+// indexOf()
+val = firstName.indexOf('D');
+val = firstName.lastIndexOf('o');
 
-        // // Type coersion Js does it for us.
-        // const val1 = String(5);
-        // const val2 = 6;
-        // const sum = val1 + val2;
+// charAt()
+val = firstName.charAt('2');
 
-        // console.log(sum);
-        // console.log(typeof sum);
-        // console.log(sum.length);
-        // console.log(sum.toFixed(2));
+//substring
+val = firstName.substring(0, 2);
 
-        // -- Property is like an attribute
+// slice
+val = firstName.slice(0, 2);
 
-// ********** 3. STRING METHODS  ***************************
+// split()
+val = str.split(' ');
 
-    // const firstName = 'Dado';
-    // const lastName = 'Steven';
-    // const str = 'Hey my name is Steven Dado Lenssen';
-    // const tags = 'web design,web development,programming';
+// replace()
+val = str.replace('Steven', 'Dado');
 
-    // let val;
-    // // Concatenation ....................................
-    // val = firstName + ' ' + lastName;
-
-    // // Append .........................
-    // val = 'Lennsen ';
-    // val += 'Steven';
-
-    // Escaping ***
-
-    // Length --- it's a property and not a method
-
-    // Concat .........................................
-    // val = firstName.concat('***', lastName);
-
-    // //Change case --- toUpperCase ... ....................
-    
-    // // indexOf
-    // val = lastName.indexOf('e'); //gives -1 if the character isn't there
-
-    // // charAt()
-    // val = lastName.charAt(2);
-    // // get last character
-    // val = lastName.charAt(lastName.length - 1);
-
-    // // Substring
-    // val = lastName.substring(0, 4);
-
-    // // Slice
-    // val = lastName.slice(0, 4);
-    // val = lastName.slice(-3);
-
-    // // Split
-    // val = str.split('');
-    // val = str.split(' ');
-    // val = str.split('  ');
-    // val = tags.split(',');
-
-    // // Replace
-    // val = str.replace('Steven', 'Lwanda');
-    // val = str.includes('ste');
-
-    // console.log(val);
-
-
-        
+// includes
+val = str.includes('Hello');
+console.log(val);
