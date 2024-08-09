ed3862320f9d47a02028012e58a3a0d593fbec10
LwandaMagere <dadolenssen@gmail.com>
2024-08-09 21:24:25 +0000

    r

---
+++ mzitoJS/JS/05_OO/app.js
@@ -0,0 +1,22 @@
+// Person constructor
+// function Person(name) {
+//     this.name = name;
+// }
+
+// const brad = new Person('James');
+// const luad = new Person('Luambo');
+
+// console.log(luad)
+////////////////////////////////////////////////////////////////////////
+
+// function Person(name) {
+//     this.name = name;
+//     console.log(this);
+// }
+
+// const name1 = new Person('Lwanda');
+// const name2 = new Person('Magere');
+//////////////////////////////////////////////////////////////////
+
+const getSum2 = new Function('x', 'y', ' return 1 + 1');
+console.log(getSum2());
