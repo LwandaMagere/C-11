fb114bd7905727a08c756c7870d62466fdc73317
LwandaMagere <dadolenssen@gmail.com>
2024-08-14 21:05:33 +0000

    r

--- mzitoJS/JS/05_OO/app.js
+++ mzitoJS/JS/05_OO/app.js
@@ -1,28 +1,59 @@
-// Constructor Method
+///////////////////// ES6 CLASSES
 
-// function Person() {
-//     this.name = "Dado";
-// }
+    // class Person {
+    //     constructor(firstName, lastName, birthday) {
+    //         this.firstName = firstName;
+    //         this.lastName = lastName;
+    //         this.birthday = new Date(1987);
+    //     }
 
-// const name = new Person();
-// const John = new Person();
+    //     greeting() {
+    //       return  `Hello there ${this.firstName} ${this.lastName}`;
+    //     }
 
-// console.log(name);
-//////////////////////////////////////////////////////////////
-    // function Person(name) {
-    //     this.name;
-    //     console.log(this);
+    //     calculateAge() {
+    //         const diff = Date.now() - this.birthday.getTime();
+    //         const ageDate = new Date(diff);
+    //         return Math.abs(ageDate.getUTCFullYear() - 1970);
+    //     }
+
+    //     static addNumber(x, y) {
+    //         return x + yield;
+    //     }
     // }
 
-    // const brad = new Person('Steven');
-    // const John = new Person('John');
-    // // console.log(brad);
+    // const mary = new Person('Dado', 'Steven', '6-6-1999');
 
-    // console.log(this); // You get the window object
-    // // alert(this)
-    // this.alert(1);
-///////////////////////////////////////////////////////
+    // console.log(mary.calculateAge());
 
 
+//////////////////// INHERITANCE / SUBCLASSES
+
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    greeting() {
+        return `Hello there ${this.firstName} ${this.lastName};`
+    }
+}
+
+// Subclass of person
+class Customer extends Person {
+    constructor(firstName, lastName, phone, membership) {
+        super(firstName, lastName);
+
+        this.phone = phone;
+        this.membership = membership;
+
+    }
+}
+
+const john = new Customer('John', 'Doe', '555-555-5555', 'standard');
+console.log(john)
+console.log(john.greeting())
 
 
+// static method is the one u can use without instatiating an object
