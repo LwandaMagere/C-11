f104e685be451d71e59de2e91b1a2dc12e75ff36
LwandaMagere <dadolenssen@gmail.com>
2024-08-17 00:19:18 +0000

    r

--- mzitoJS/JS/tasklist/app.js
+++ mzitoJS/JS/tasklist/app.js
@@ -6,28 +6,30 @@ const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
 // Load all event listeners
-loadEvenListeners();
+loadEventListeners();
 
-function loadEvenListeners() {
-    // Add task event
+function loadEventListeners() {
+    // Add task Event
     form.addEventListener('submit', addTask);
 }
 
-// Add task
+// Add Task
 function addTask(e) {
-    if (taskInput.value === '') {
-        alert('Add Task');
+    if(taskInput.value === '') {
+        alert('Add a task');
     }
+
+    // Create li element
     const li = document.createElement('li');
-    li.className = 'collection-item';
+    li.className  = 'collection-item';
+    // Create text node and append to the child
     li.appendChild(document.createTextNode(taskInput.value));
     const link = document.createElement('a');
     link.className = 'delete-item secondary-content';
-    link.innerHTML = '<i class="fa fa-remove"></i>';
+    link.innerHTML = '<i class="fa fa-remove"></i>'
     li.appendChild(link);
     taskList.appendChild(li);
 
-    // clear input
     taskInput.value = '';
 
     e.preventDefault();
