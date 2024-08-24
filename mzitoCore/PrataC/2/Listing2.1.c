021fea3085a7ec85621f7149c0ff6b9d5463a0a1
LwandaMagere <dadolenssen@gmail.com>
2024-08-24 04:10:46 +0000

    R

--- mzitoCore/PrataC/2/Listing2.1.c
+++ mzitoCore/PrataC/2/Listing2.1.c
@@ -4,9 +4,9 @@ int main(void)
     int num;
     num = 1;
 
-    printf("I am a simple "); 
+    printf("I am a simple ");
     printf("computer.\n");
     printf("My favorite number is %d because it is first.\n",num);
-
+    
     return 0;
 }
