be9b84aaa12e74e8b94f3c417ea0dc1e88463962
LwandaMagere <dadolenssen@gmail.com>
2024-08-24 03:53:31 +0000

    R

--- mzitoCore/PrataC/1/Listing1.1.c
+++ mzitoCore/PrataC/1/Listing1.1.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 int main(void) 
 {
     int dogs;
 
     printf("How many dogs do you have?\n");
     scanf("%d", &dogs);
-    printf("So you have %d dog(s)!\n", dogs);
+    printf("So you have %d dogs(s)!\n", dogs);
     
     return 0;
 }
