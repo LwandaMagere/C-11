d61aa4c93b8d84c7132044662b61f5d38c3d9938
LwandaMagere <dadolenssen@gmail.com>
2024-08-24 23:46:59 +0000

    r

--- mzitoCore/PrataC/3/Listing3.6.c
+++ mzitoCore/PrataC/3/Listing3.6.c
@@ -1,9 +1,9 @@
 /* altnames.c -- portable names for integer types */
 #include <stdio.h>
-#include <inttypes.h> // supports portable types
+#include <inttypes.h>  // supports portable types
 int main(void) 
 {
-    int32_t me32;
+    int32_t me32;  // me32 a 32-bit signed variable
 
     me32 = 45933945;
     printf("First, assume int32_t is int: ");
@@ -11,7 +11,6 @@ int main(void)
     printf("Next, let's not make any assumptions.\n");
     printf("Instead, use a \"macro\" from inttypes.h: ");
     printf("me32 = %" PRId32 "\n", me32);
-
 
     return 0;
 }
