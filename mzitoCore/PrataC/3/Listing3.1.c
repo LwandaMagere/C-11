4d6f6ce39b0251c79061bf3019826d942253a4d9
LwandaMagere <dadolenssen@gmail.com>
2024-08-24 09:34:48 +0000

    r

--- mzitoCore/PrataC/3/Listing3.1.c
+++ mzitoCore/PrataC/3/Listing3.1.c
@@ -8,8 +8,7 @@ int main(void)
     printf("Are you worth your weight in platinum?\n");
     printf("Let's check it out.\n");
     printf("Please enter your weight in pounds: ");
-
-    // get input from the user
+    /* get input from the user */
     scanf("%f", &weight);
     /* assume platinum is $1700 per ounce */
     /* 14.5833 converts pounds avd. to ounces troy */
@@ -17,6 +16,8 @@ int main(void)
     printf("Your weight in platinum is worth $%.2f.\n", value);
     printf("You are easily worth that! If platinum prices drop,\n");
     printf("eat more to maintain your value.\n");
+
+
 
     return 0;
 }
