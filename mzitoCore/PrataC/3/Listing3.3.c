9a4706ff6f1e5a038086ac80ae0c87c2d4e29b54
LwandaMagere <dadolenssen@gmail.com>
2024-08-24 23:19:15 +0000

    r

--- mzitoCore/PrataC/3/Listing3.3.c
+++ mzitoCore/PrataC/3/Listing3.3.c
@@ -4,8 +4,8 @@ int main(void)
 {
     int x = 100;
 
-    printf("dec = %d; octal = %o; hex = %x\n", x, x, x);
-    printf("dec = %d; octal = %#o; hex = %#x\n", x, x, x);
+    printf("dec = %d; octol = %o; hex = %x\n", x ,x ,x);
+    printf("dec = %d; octal = %#o; hex = %#x", x, x, x);
     
     return 0;
 }
