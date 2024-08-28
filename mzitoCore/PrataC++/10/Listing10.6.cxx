404ac31c80265fa859b3ce3ee8b84164e6dc8a53
LwandaMagere <dadolenssen@gmail.com>
2024-08-28 14:37:57 +0000

    r

--- mzitoCore/PrataC++/10/Listing10.6.cxx
+++ mzitoCore/PrataC++/10/Listing10.6.cxx
@@ -3,14 +3,14 @@
 #include <iostream>
 #include "Listing10.4.h"
 
-int main()
+int main() 
 {
     {
         using std::cout;
         cout << "Using constructors to create new objects\n";
-        Stock stock1("NanoSmart", 12, 20); // syntax 1;
+        Stock stock1("NanoSmart", 20, 12.50);   // syntax 1
         stock1.show();
-        Stock stock2 = Stock("Boffo Objects", 2, 2.0); // syntax 2
+        Stock stock2 = Stock("Boffo Objects", 2, 2.0);
         stock2.show();
 
         cout << "Assigning stock1 to stock2:\n";
@@ -22,9 +22,9 @@ int main()
         cout << "Using a constructor to reset an object\n";
         stock1 = Stock("Nifty Foods", 10, 50.0); // temp object
         cout << "Revised stock1:\n";
+
         stock1.show();
         cout << "Done\n";
-
     }
     return 0;
 }
