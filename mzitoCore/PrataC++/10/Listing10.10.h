566fd511df8bf29c86560d9611be3057833448a6
LwandaMagere <dadolenssen@gmail.com>
2024-08-28 20:58:03 +0000

    r

--- mzitoCore/PrataC++/10/Listing10.10.h
+++ mzitoCore/PrataC++/10/Listing10.10.h
@@ -2,25 +2,22 @@
 #ifndef STACK_H_
 #define STACK_H_
 
-#include <iostream>
-using namespace std;
-
 typedef unsigned long Item;
 
-class Stack
+class Stack 
 {
     private:
-        enum {MAX};         // constant specific to class
-        Item items[MAX]; // holds stack items
+        enum {MAX = 10};  // constant specific to class
+        Item items[MAX];  // holds stack items
         int top;        // index for top stack item
     public:
         Stack();
-        bool isempty()const;
+        bool isempty() const;
         bool isfull() const;
         // push() returns false if stack already is full, true otherwise
         bool push(const Item & item); // add item to stack
         // pop() returns false if stack already is empty, true otherwise
-        bool pop(Item & item);  // pop top into item
+        bool pop(Item & item);   // pop top into item
 
 };
 
