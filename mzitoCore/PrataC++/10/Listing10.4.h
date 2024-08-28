60fc1d7fa42cb5bcf55332d42c8ff034171eddbe
LwandaMagere <dadolenssen@gmail.com>
2024-08-28 14:27:10 +0000

    r

--- mzitoCore/PrataC++/10/Listing10.4.h
+++ mzitoCore/PrataC++/10/Listing10.4.h
@@ -1,9 +1,7 @@
 // stock10.h -- Stock class declaration with constructors, destructor added
 #ifndef STOCK10_H_
 #define STOCK10_H_
-
 #include <string>
-#include <iostream>
 
 class Stock
 {
@@ -12,16 +10,17 @@ class Stock
         long shares;
         double share_val;
         double total_val;
-        void set_tot() { total_val = shares * share_val; }
+        void set_tot() { total_val = shares * share_val;}
     public:
-        // two constructors
-        Stock(); // default constructors
-        Stock(const std::string & co, long n = 0, double pr = 0.0);
-        ~Stock();  // noisy destructor
+        Stock();
+        Stock(const std::string &co, long n = 0, double pr = 0.0);
+        ~Stock();
         void buy(long num, double price);
         void sell(long num, double price);
         void update(double price);
         void show();
+
 };
+
 
 #endif
