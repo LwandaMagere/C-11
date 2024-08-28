d1890de4d0fe815f06b2802da6e2fa9bf9ea6dd1
LwandaMagere <dadolenssen@gmail.com>
2024-08-28 11:24:20 +0000

    r

--- mzitoCore/PrataC++/10/Listing10.3.cxx
+++ mzitoCore/PrataC++/10/Listing10.3.cxx
@@ -1,12 +1,12 @@
 // usestck0.cpp -- the client program
 // compile with stock00.cpp
-
+#include <iostream>
 #include "Listing10.1.h"
 
-int main() 
+int main()
 {
     Stock fluffy_the_cat;
-    fluffy_the_cat.acquire("NanoSmart", 20, 12.50);
+    fluffy_the_cat.acquire("Johari Jars", 20, 12.50);
     fluffy_the_cat.show();
     fluffy_the_cat.buy(15, 18.125);
     fluffy_the_cat.show();
