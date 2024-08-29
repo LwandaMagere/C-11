a9663f9672a2d0d456b58bdfe155575d8743e9b9
LwandaMagere <dadolenssen@gmail.com>
2024-08-29 01:12:34 +0000

    r

--- mzitoCore/PrataC++/12/Listing12.9.cxx
+++ mzitoCore/PrataC++/12/Listing12.9.cxx
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <string>
 #include <new>
-using namespace std;
 
+using namespace std;
 const int BUF = 512;
 
-class JustTesting
+class JustTesting 
 {
     private:
         string words;
@@ -18,17 +18,17 @@ class JustTesting
         void Show() const { cout << words << ", " << number << endl;}
 };
 
-int main()
+int main() 
 {
-    char * buffer = new char[BUF];  // get a block of memory
+    char * buffer = new char[BUF];
 
     JustTesting *pc1, *pc2;
 
-    pc1 = new (buffer) JustTesting;
-    pc2 = new JustTesting("Heap", 30);
-
+    pc1 = new (buffer) JustTesting;  // place object in buffer
+    pc2 = new JustTesting("heap", 20);  // place object on heap
     cout << "Memory block addresses:\n" << "buffer: "
-         << (void *) buffer << " heap: " << pc2 <<endl;
+         <<  (void *)buffer << " heap: " << pc2 <<endl;
+
     cout << "Memory contents:\n";
     cout << pc1 << ": ";
     pc1->Show();
@@ -37,7 +37,7 @@ int main()
 
     JustTesting *pc3, *pc4;
     // fix placement new location
-    pc3 = new (buffer + sizeof(JustTesting)) JustTesting("Better Idea", 6);
+    pc3 = new (buffer + sizeof (JustTesting)) JustTesting("Better Idea", 6);
     pc4 = new JustTesting("Heap2", 10);
 
     cout << "Memory contents:\n";
@@ -45,16 +45,14 @@ int main()
     pc3->Show();
     cout << pc4 << ": ";
     pc4->Show();
-
-    delete pc2; // free Heap1
-    delete pc4; // free Heap2
-
+    delete pc2;  // free Heap1
+    delete pc4;  // free Heap2
     // explicitly destroy placement new objects
     pc3->~JustTesting();  // destroy object pointed to by pc3
     pc1->~JustTesting();  // destroy object pointed to by pc1
-    
-    delete [] buffer;  // free buffe
+    delete [] buffer; // free buffer
     cout << "Done\n";
 
     return 0;
 }
+
