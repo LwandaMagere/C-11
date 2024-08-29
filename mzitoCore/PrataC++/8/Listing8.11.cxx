efe1e765df888f481de763feee94f350ea65187a
LwandaMagere <dadolenssen@gmail.com>
2024-08-29 17:05:22 +0000

    r

--- mzitoCore/PrataC++/8/Listing8.11.cxx
+++ mzitoCore/PrataC++/8/Listing8.11.cxx
@@ -1,8 +1,8 @@
 // funtemp.cpp -- using a function template
 #include <iostream>
 // function template prototype
-template<typename T>   // or class T
-void Swap(T &a, T &b); 
+template <typename T> // or class T
+void Swap(T &a, T &b);
 
 int main() 
 {
@@ -22,13 +22,14 @@ int main()
     Swap(x,y); // generates void Swap(double &, double &)
     cout << "Now x, y = " << x << ", " << y << ".\n";
     // cin.get();
-    
+
+
     return 0;
 }
 
-// function template defination
-template<typename T>
-void Swap(T & a, T & b) 
+// function template definition
+template <typename T>
+void Swap(T &a, T &b) 
 {
     T temp;
     temp = a;
