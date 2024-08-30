cc835c47a24d045b83a05b0cfdc5accc869affc6
LwandaMagere <dadolenssen@gmail.com>
2024-08-30 19:23:44 +0000

    r

--- mzitoCore/PrataC++/10/Listing10.10.h
+++ mzitoCore/PrataC++/10/Listing10.10.h
@@ -9,16 +9,16 @@ class Stack
     private:
         enum {MAX = 10};  // constant specific to class
         Item items[MAX];  // holds stack items
-        int top;        // index for top stack item
+        int top; // index for top stack
     public:
         Stack();
         bool isempty() const;
         bool isfull() const;
         // push() returns false if stack already is full, true otherwise
-        bool push(const Item & item); // add item to stack
+        bool push(const Item & items);  // add item to stack
         // pop() returns false if stack already is empty, true otherwise
-        bool pop(Item & item);   // pop top into item
-
+        bool pop(Item & item); // pop top into item
 };
+
 
 #endif
