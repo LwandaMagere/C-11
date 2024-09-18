186687d24d2748ea26e5fd30b2469c61706f8a2e
LwandaMagere <dadolenssen@gmail.com>
2024-09-18 16:18:54 +0000

    r

--- mzitoJS/JS/04_EventBubling/app.js
+++ mzitoJS/JS/04_EventBubling/app.js
@@ -132,6 +132,34 @@
 
 
 
+document.querySelector('form').addEventListener('submit', function(e){
+
+    const task = document.getElementById('task').value;
+
+    let tasks;
+
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task)
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    alert('Task Saved');
+    e.preventDefault();
+});
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+tasks.forEach(function(task, index){
+console.log(task)
+})
+
+
+
+
 
 
 
