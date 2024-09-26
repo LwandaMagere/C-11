85a6c7e2974d1e4bab184c139db9d14ff61448ca
LwandaMagere <dadolenssen@gmail.com>
2024-09-26 11:50:47 +0000

    r

---
+++ Python/1_Intro/first_script.py
@@ -0,0 +1,9 @@
+# A first python script
+import sys        # Load a library module
+print(sys.platform)
+print(2 ** 100)
+X = 'spam'
+print(X * 8)
+
+# The sys.platform here is just a string that 
+# identifies the kind of computer you’re working on
