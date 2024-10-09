3f7e5ddf34ca11426fc0e0f01cdf91cf93e122b4
LwandaMagere <dadolenssen@gmail.com>
2024-10-09 09:43:53 +0000

    r

--- mzitoCore/PrataC++/8/Listing8.12.cxx
+++ mzitoCore/PrataC++/8/Listing8.12.cxx
@@ -1,22 +1,22 @@
 // twotemps.cpp -- using overloaded template functions
 #include <iostream>
 
-template<class T>       // original template
-void Swap(T &a, T &b);
+template<typename T>
+void Swap(T &, T &);
 
-template<typename T>        // new template
-void Swap(T * a, T *b, int n);
+template <typename T>
+void Swap(T * , T * , int n = 1);
 
-void Show(  int a[]);
+void Show(int a[]);
 const int Lim = 8;
 
 int main()
 {
-     using namespace std;
+    using namespace std;
     int i = 10, j = 20;
     cout << "i, j = " << i << ", " << j << ".\n";
     cout << "Using compiler-generated int swapper:\n";
-    Swap(i,j);      // matches original template
+    Swap(i,j); // matches original template
     cout << "Now i, j = " << i << ", " << j << ".\n";
 
     int d1[Lim] = {0,7,0,4,1,7,7,6};
@@ -24,30 +24,27 @@ int main()
     cout << "Original arrays:\n";
     Show(d1);
     Show(d2);
-    Swap(d1,d2,Lim);        // matches new template
+    Swap(d1,d2,Lim); // matches new template
     cout << "Swapped arrays:\n";
     Show(d1);
     Show(d2);
-    // cin.get();
-
-
     return 0;
 }
 
-template<typename T>
-void Swap(T &a, T &b) 
+template <typename T>
+void Swap(T & a, T & b) 
 {
     T temp;
     temp = a;
-    a  = b;
+    a = b;
     b = temp;
 }
 
-template<class T>
-void Swap(T a[], T b[]) 
+template<typename T>
+void Swap(T a[], T b[], int n) 
 {
     T temp;
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
     {
         temp = a[i];
         a[i] = b[i];
@@ -61,6 +58,13 @@ void Show(int a[])
     cout << a[0] << a[1] << "/";
     cout << a[2] << a[3] << "/";
     for (int i = 4; i < Lim; i++)
-        cout << a[i];
+    cout << a[i];
     cout << endl;
 }
+
+
+
+
+
+//You use templates when you need functions that apply the same algorithm to a variety of types
+//It might be, however, that not all types would use the same algorithm.
