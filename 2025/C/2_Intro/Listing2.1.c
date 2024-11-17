e8365d1645a5443904cda2452bf8803c8fcfab86
LwandaMagere <dadolenssen@gmail.com>
2024-11-17 19:42:18 +0300

    ratuombi

---
+++ 2025/C/2_Intro/Listing2.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;        /* define a variable called num */
+    num = 1;        /* assign a value to num */
+
+    printf("I am a simple ");
+    printf("Computer.\n");
+    printf("My favorite number is %d because it is first.\n", num);
+
+
+    return 0;
+}
