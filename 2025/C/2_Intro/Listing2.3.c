934ac5771ac51a534a5c735df7261ebf7eabf2f1
LwandaMagere <dadolenssen@gmail.com>
2024-11-17 20:46:34 +0300

    ratuombi

---
+++ 2025/C/2_Intro/Listing2.3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void butler(void);   /* ANSI/ISO C function prototyping */
+
+int main(void) 
+{
+    printf("I will summon the butler function.\n");
+    butler();
+    printf("Yes. Bring me some tea and writeable DVDs.\n");
+
+    return 0;
+}
+
+void butler(void)   /* start of function definition */
+{
+    printf("You rang, sir?\n");
+}
