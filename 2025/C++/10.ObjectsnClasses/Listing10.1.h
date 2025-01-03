0107059fb8800e0407c443342de870d36524a9e8
LwandaMagere <dadolenssen@gmail.com>
2025-01-03 10:05:34 +0300

    r

---
+++ 2025/C++/10.ObjectsnClasses/Listing10.1.h
@@ -0,0 +1,21 @@
+#ifndef STOCK00_H_
+#define STOCK00_H_
+
+#include <string>
+
+class Stock
+{
+    private:
+        std::string company;
+        long shares;
+        double total_val;
+        double share_val;
+        void set_tot() {total_val = shares * share_val;}
+    public:
+        void acquire(const std::string & co, long n, double pr);
+        void buy(long num, double price);
+        void sell(long num, double price);
+        void show();
+};
+
+#endif
