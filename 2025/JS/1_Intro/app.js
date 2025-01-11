4d8734a930cc59b9949089e2f2af652b6192fcd0
dadolenssen@gmail.com <dadolenssen@gmail.com>
2025-01-11 16:19:01 +0300

    r

--- 2025/JS/1_Intro/app.js
+++ 2025/JS/1_Intro/app.js
@@ -1,1 +1,23 @@
-alert("Hello World!");
+// alert("Hello World!");
+
+// LOG TO CONSOLE
+console.log("Hello World");
+console.log(123);
+console.log(true);
+let greetings = 'Hello World';
+console.log(greetings);
+console.log([1,2,3,45,5]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning!');
+
+console.time('Hello');
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+console.timeEnd('Hello');
