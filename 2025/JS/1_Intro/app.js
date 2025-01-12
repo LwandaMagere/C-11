4f3310b52715aa13d096b4b0efdcb7e9a8596b3c
dadolenssen@gmail.com <dadolenssen@gmail.com>
2025-01-12 11:13:26 +0300

    r

--- 2025/JS/1_Intro/app.js
+++ 2025/JS/1_Intro/app.js
@@ -57,3 +57,32 @@ console.log(typeof address);
 
 const date = new Date();
 console.log(typeof date);
+
+let val;
+
+// Number to string
+val = String(5);
+val = String(5+5);
+// Bool to string
+val = String(val);
+// Date to a string
+val = String(new Date());
+// Array to string
+val = String([1,2,3,4]);
+
+// toString
+val = (5).toString();
+val = (true).toString();
+
+// String to Numbers
+val = Number('5');
+val = parseInt('100');
+val = parseFloat('100.33');
+
+
+
+// Output
+console.log(val);
+console.log(typeof val);
+// console.log(val.length);
+console.log(val.toFixed());
