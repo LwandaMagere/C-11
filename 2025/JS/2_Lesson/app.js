1928d43012f62c9af70ced7e8d6e72cd5b50f437
dadolenssen@gmail.com <dadolenssen@gmail.com>
2025-02-02 09:28:39 +0300

    r

--- 2025/JS/2_Lesson/app.js
+++ 2025/JS/2_Lesson/app.js
@@ -25,57 +25,83 @@
 
 // document.body.innerHTML = html;
 
-// 2. ARRAYS AND ARRAY METHODS
+// 2. ARRAYS AND ARRAY METHODS ////////////////////////////////////
 
-// create some arrays
-const numbers = [43,56, 33, 23, 44, 36, 5];
-const numbers2 = new Array(22,45,33,75, 54);
-const fruit = ['apple', 'banana', 'apple', 'pear'];
-const mixed = [22, 'hello', true, undefined, null, {a: 1, b: 2}, new Date()];
+// // create some arrays
+// const numbers = [43,56, 33, 23, 44, 36, 5];
+// const numbers2 = new Array(22,45,33,75, 54);
+// const fruit = ['apple', 'banana', 'apple', 'pear'];
+// const mixed = [22, 'hello', true, undefined, null, {a: 1, b: 2}, new Date()];
+
+// let val;
+
+// // Get array length
+// val = numbers.length;
+// // Check if is array
+// val = Array.isArray(numbers);
+// // Find index of value
+// val = numbers.indexOf(36);
+
+// // Mutating Arrays
+
+// // // Add on to end
+// // numbers.push(250);
+// // // Add on to front
+// // numbers.unshift(120);
+// // // Take off from end
+// // numbers.pop();
+// // // Take off from front
+// // numbers.shift();
+// // // Splice
+// // numbers.splice(1,1);
+// // // Reverse
+// // numbers.reverse();
+
+// // Concatenate arrays
+// val = numbers.concat(numbers2);
+
+// // Sorting arrays
+// val = fruit.sort()
+
+// // Use the "compare function"
+// val = numbers.sort(function(x, y){
+//     return x - y;
+// });
+
+// // Find
+// function under50(num) {
+//     return num < 50;
+// }
+
+// val = numbers.find(under50);
+
+
+// console.log(numbers);
+// console.log(val);
+
+// 3. OBJECT LITERALS ///////////////////////////////
+
+const person = {
+    firstName: 'Steve',
+    lastName: 'Smith',
+    age: 30,
+    email: 'steve@gmail.com',
+    hobbies: ['music', 'sports'],
+    address: {
+        city: 'Nairobi',
+        state: 'Florida'
+    },
+    getBirthYear: function(){
+        return 1987;
+    }
+}
 
 let val;
 
-// Get array length
-val = numbers.length;
-// Check if is array
-val = Array.isArray(numbers);
-// Find index of value
-val = numbers.indexOf(36);
+val = person;
 
-// Mutating Arrays
-
-// // Add on to end
-// numbers.push(250);
-// // Add on to front
-// numbers.unshift(120);
-// // Take off from end
-// numbers.pop();
-// // Take off from front
-// numbers.shift();
-// // Splice
-// numbers.splice(1,1);
-// // Reverse
-// numbers.reverse();
-
-// Concatenate arrays
-val = numbers.concat(numbers2);
-
-// Sorting arrays
-val = fruit.sort()
-
-// Use the "compare function"
-val = numbers.sort(function(x, y){
-    return x - y;
-});
-
-// Find
-function under50(num) {
-    return num < 50;
-}
-
-val = numbers.find(under50);
+// Get specific value
+val = person.firstName;
 
 
-console.log(numbers);
 console.log(val);
-
