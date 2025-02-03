49e886a946aee3e81ba0202147371bf30eb84235
dadolenssen@gmail.com <dadolenssen@gmail.com>
2025-02-03 15:50:40 +0300

    r

---
+++ 2025/JS/3_Lesson/app.js
@@ -0,0 +1,31 @@
+// 1. WHAT IS THE DOM (Document Object Model) ?
+
+/* 
+
+- A structured representation of an html document
+-Tree of nodes/elements created by the browser.
+-Javascript can be used to read/write/manipulate the dom
+-Object Oriented representation
+
+*/
+
+let val;
+
+val = document;
+val = document.all;
+val = document.all[2];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+console.log(val);
+
+
+
+
+
