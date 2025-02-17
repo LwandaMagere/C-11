23cdf106ad0b97e648c7c095958204f6d94a9bd7
dadolenssen@gmail.com <dadolenssen@gmail.com>
2025-02-17 12:57:19 +0300

    r

--- 2025/JS/1_Intro/app.js
+++ 2025/JS/1_Intro/app.js
@@ -1,90 +1,21 @@
-// alert("Hello World!");
+// alert('Hello World!');
 
-// LOG TO CONSOLE
-console.log("Hello World");
+// Log to console
+console.log('sasa');
 console.log(123);
 console.log(true);
-let greetings = 'Hello World';
-console.log(greetings);
-console.log([1,2,3,45,5]);
+let greeting = 'Hello World!';
+console.log(greeting);
+console.log([1,2,3]);
 console.log({a:1, b:2});
 console.table({a:1, b:2});
-
 console.error('This is some error');
 console.clear();
-console.warn('This is a warning!');
+console.warn('This is warning!');
 
 console.time('Hello');
-    console.log("Hello World");
-    console.log("Hello World");
-    console.log("Hello World");
-    console.log("Hello World");
-    console.log("Hello World");
+    console.log('sasa');
+    console.log('sasa');
+    console.log('sasa');
+    console.log('sasa');
 console.timeEnd('Hello');
-console.clear();
-
-// PRIMITIVE TYPES
-
-// String
-const name = 'John Doe';
-console.log(typeof name);
-// Number
-const age = 42;
-console.log(typeof age);
-// Boolean
-const hasKids = true;
-console.log(typeof hasKids);
-// Null
-const car = null;
-console.log(typeof car);
-// Undefined
-let test;
-console.log(typeof test);
-// Symbol
-const sym = Symbol();
-console.log(typeof sym);
-
-// REFERENCE TYPES - Objects
-// Arrays
-const hobbies = ['music', 'footbak', 'singing'];
-console.log(typeof hobbies);
-// Object literal
-const address = {
-    city: 'Boston',
-    state: 'MA'
-};
-console.log(typeof address);
-
-const date = new Date();
-console.log(typeof date);
-
-let val;
-
-// Number to string
-val = String(5);
-val = String(5+5);
-// Bool to string
-val = String(val);
-// Date to a string
-val = String(new Date());
-// Array to string
-val = String([1,2,3,4]);
-
-// toString
-val = (5).toString();
-val = (true).toString();
-
-// String to Numbers
-val = Number('5');
-val = parseInt('100');
-val = parseFloat('100.33');
-
-
-
-// Output
-console.log(val);
-console.log(typeof val);
-// console.log(val.length);
-console.log(val.toFixed());
-
-
