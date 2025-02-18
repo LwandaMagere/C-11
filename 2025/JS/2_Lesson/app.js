c10f45de6d5d880ba854f42e19b02b027541ab06
dadolenssen@gmail.com <dadolenssen@gmail.com>
2025-02-18 10:06:43 +0300

    r

--- 2025/JS/2_Lesson/app.js
+++ 2025/JS/2_Lesson/app.js
@@ -1,170 +1,25 @@
-// 1. TEMPLATE LITERALS
+const name = 'John';
+const age = 30;
+const job = 'Web developer';
+const city = 'Miami';
+let html;
 
-// const name = 'John';
-// const age = 30;
-// const job = 'web developer';
-// const city = 'Miami';
-// let html;
+function hello() 
+{
+    return 'Hello!';
+}
 
-// function hello() {
-//     return 'hello';
-// }
+// Template literals/strings
+html = `
+    <ul>
+        <li>Name: ${name}</li>
+        <li>Age: ${age}</li>
+        <li>Job: ${job}</li>
+        <li>City: ${city}</li>
+        <li>${2 + 2}</li>
+        <li>${hello()}</li>
+        <li>${age < 30 ? 'under 30' : 'over 30'}</li>
+    </ul>
+`;
 
-// // With template strings / literal
-// html = `
-// <ul>
-//     <li>Name: ${name}</li>
-//     <li>Age: ${age}</li>
-//     <li>Job: ${job}</li>
-//     <li>City: ${city}</li>
-//     <li>${2 + 2}</li>
-//     <li>${hello()}</li>
-//     <li>${age > 30 ? 'over 30' : 'under 30'}</li>
-// </ul>
-// `;
-
-// document.body.innerHTML = html;
-
-// 2. ARRAYS AND ARRAY METHODS ////////////////////////////////////
-
-// // create some arrays
-// const numbers = [43,56, 33, 23, 44, 36, 5];
-// const numbers2 = new Array(22,45,33,75, 54);
-// const fruit = ['apple', 'banana', 'apple', 'pear'];
-// const mixed = [22, 'hello', true, undefined, null, {a: 1, b: 2}, new Date()];
-
-// let val;
-
-// // Get array length
-// val = numbers.length;
-// // Check if is array
-// val = Array.isArray(numbers);
-// // Find index of value
-// val = numbers.indexOf(36);
-
-// // Mutating Arrays
-
-// // // Add on to end
-// // numbers.push(250);
-// // // Add on to front
-// // numbers.unshift(120);
-// // // Take off from end
-// // numbers.pop();
-// // // Take off from front
-// // numbers.shift();
-// // // Splice
-// // numbers.splice(1,1);
-// // // Reverse
-// // numbers.reverse();
-
-// // Concatenate arrays
-// val = numbers.concat(numbers2);
-
-// // Sorting arrays
-// val = fruit.sort()
-
-// // Use the "compare function"
-// val = numbers.sort(function(x, y){
-//     return x - y;
-// });
-
-// // Find
-// function under50(num) {
-//     return num < 50;
-// }
-
-// val = numbers.find(under50);
-
-
-// console.log(numbers);
-// console.log(val);
-
-// 3. OBJECT LITERALS ///////////////////////////////
-
-// const person = {
-//     firstName: 'Steve',
-//     lastName: 'Smith',
-//     age: 30,
-//     email: 'steve@gmail.com',
-//     hobbies: ['music', 'sports'],
-//     address: {
-//         city: 'Nairobi',
-//         state: 'Florida'
-//     },
-//     getBirthYear: function(){
-//         return 1987;
-//     }
-// }
-
-// let val;
-
-// val = person;
-
-// // Get specific value
-// val = person.firstName;
-
-
-// console.log(val);
-
-
-//// 4. LOOP THROUGH ARRAY
-// const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
-
-// for (let i = 0; i < cars.length; i++) 
-// {
-//         console.log(cars[i]);
-// }
-
-// // FOREACH
-// cars.forEach(function(car){
-//     console.log(car)
-// });
-
-// // MAP
-// const users = [
-//     {id: 1, name: 'John'},
-//     {id: 2, name: 'Sara'},
-//     {id: 3, name: 'Karen'}
-// ]
-
-// const ids = users.map(function(user){
-//     return user.id;
-// })
-
-// // FOR IN LOOP
-// const user = {
-//     firstName: 'John',
-//     lastName: 'Doe',
-//     age: 40
-// }
-
-// for (let x in user) {
-//     console.log(`${x} : ${user[x]}`);
-// }
-
-// WINDOW METHODS / OBJECTS / PROPERTIES
-
-// Alert
-// window.alert('Hello World');
-
-// Prompt
-// const input = prompt();
-// alert(input);
-
-// Confirm
-// if (confirm('Are you sure?')) {
-//     console.log('Yes')
-// }
-
-let val;
-
-// Outer height and Width
-val = window.outerHeight;
-
-// Scroll Points
-val = window.scrollY;
-
-// Location Object
-val = window.location;
-
-console.log(val)
+document.body.innerHTML = html;
