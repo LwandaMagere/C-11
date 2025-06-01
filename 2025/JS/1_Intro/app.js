25aae85368bebfcbaf8993d330f1336d1346dc04
dadolenssen@gmail.com <dadolenssen@gmail.com>
2025-06-01 13:46:11 +0300

    r

--- 2025/JS/1_Intro/app.js
+++ 2025/JS/1_Intro/app.js
@@ -1,120 +1,172 @@
-// alert('Hello World!');
+// alert("Hello World!");
 
-// 1. Log to console
-// console.log('sasa');
+// -- Using the Console
+
+// Log to console
+// console.log("Hello World!");
 // console.log(123);
 // console.log(true);
-// let greeting = 'Hello World!';
+// var greeting = 'Hello';
 // console.log(greeting);
-// console.log([1,2,3]);
+// console.log([1,2,3,4]);
 // console.log({a:1, b:2});
 // console.table({a:1, b:2});
-// console.error('This is some error');
+
+// console.error('This is some error!');
 // console.clear();
-// console.warn('This is warning!');
+// console.warn('This is a warning!');
 
 // console.time('Hello');
-//     console.log('sasa');
-//     console.log('sasa');
-//     console.log('sasa');
-//     console.log('sasa');
+//     console.log("Hello World!");
+//     console.log("Hello World!");
+//     console.log("Hello World!");
+//     console.log("Hello World!");
+//     console.log("Hello World!");
 // console.timeEnd('Hello');
 
-// 2. Variables and Declarations
 
-// let, const
+// VARIABLES -- let, const
+
+// Init a variable
+// let a;
+// console.log(a);
+// a = 'Hello';
+// console.log(a);
 
 // const person = {
 //     name: 'John',
-//     age: 40
-// };
+//     age: 30
+// }
 
-// person.name = 'sara';
+// person.name = 'Sara';
 // console.log(person);
 
-// 3. Types of Data
+// const numbers = [1,2,3,4,5];
+// numbers.push(6);
+// console.log(numbers);
 
-/*
--------- Primitive Data Types
-STRINGS
-NUMBERS
-SYMBOLS
-BOOLEAN
-NULL
-UNDEFINED
+// DATA TYPES
 
-----------Reference Types
-ARRAYS
-OBJECT LITERALS
-FUNCTIONS
-DATES
-ANYTHING ELSE
+// -- Primitive Data types
+// Strings, Numbers, Symbols, Null, Boolean, Undefined
 
-*/
-
-// PRIMITIVES
-
-// Strings
-const name = 'John Doe';
-// Numbers
-const age = 45;
-// Boolean
-const hasKids = true;
-// Null
-const car = null;
-//undefined
-let test;
-// Symbol
-const sym = Symbol();
-
-// REFERENCE - Objects
-
-// Arrays
-const hobbies = ['movies', 'swimming', 'reading'];
-// Object literal
-const address = {
-    city: 'Boston',
-    state: 'MA'
-};
-// Date
-const date = new Date();
+// Reference Types
+// Arrays, Object literals, Functions, Dates, anything else
 
 
+// PRIMITIVE TYPES
 
-console.log(typeof date);
+// String
+// const name = 'John Doe';
+// // Number
+// const age = 45;
+// // Boolean
+// const hasKids = true;
+// // Null
+// const car = null;
+// // Undefined
+// let test;
+// //Symbol
+// const sym = Symbol();
 
-// 4. Type Conversion
+// // REFERENCE TYPES - Object
 
-// Number to String
+// // Arrays
+// const hobbies = ['movies', 'music']
+// // Object literal
+// const address = {
+//     city: 'Boston',
+//     state: 'MA',
+// }
+// const today = new Date();
+// console.log(today);
+
+// console.log(typeof today);
+
+// TYPE CONVERSION
+
+// let val;
+
+// // Number to String
+// val = String(5);
+// val = String(4 + 4);
+// // Bool to string
+// val = String(true);
+// // Date to string
+// val = String(new Date());
+// // Array to String
+// val = String([1,2,3,4,5]);
+
+// // toString()
+// val = (5).toString();
+// val = (true).toString();
+
+// String to number
+// val = Number('5');
+// val = Number(true);
+// val = Number(null);
+// val = Number('Hello');
+// val = Number([1,3,5]);
+
+// val = parseInt('100');
+// val = parseFloat('100.30');
+
+
+// // Output
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length);
+// console.log(val.toFixed());
+
+// TYPE COERSION -- It happend automatically
+
+// let val = Math.PI;
+// console.log(val);
+
+// val = Math.floor(Math.random() * 20 + 1);
+// console.log(val);
+
+// STRING
+
+const firstName = 'William';
+const lastName = 'Johnson';
+const str = 'Hello there my name is Brad';
+
 let val;
 
-val = String(555);
-val = String(4+4);
+val = firstName + lastName;
 
-// Boolean to String
-val = String(true);
+// Concatenation
+val = firstName + ' ' + lastName;
+val = firstName.concat(' ', lastName);
+// Append
+val = 'Brad ';
+val += 'Traversy';
 
-// Date to String
-val = String(new Date());
+val = firstName.indexOf('l');
+val = lastName.lastIndexOf('l');
 
-// Array to String
-val = String([1,2,3,4]);
+// charAt
+val = firstName.charAt('2');
+// Get last character
+val = firstName.charAt(firstName.length - 1);
+// Sub String
+val = firstName.substring(0, 4);
+// Slice
+val = firstName.slice(0, 4);
+val = firstName.slice(-3);
 
-// toString()
-val = (5).toString();
-val = (true).toString();
+// split()
+val = str.split(' ');
+// replace
+val = str.replace('Brad', 'Jack');
+// includes
+val = str.includes('Hello');
 
-// String to Numbers
-val = Number('5');
-val = Number(true);
-val = Number(null);
-val = Number('Hello');
-val = Number([1,2,3]);
-
-val = parseInt('100');
 
 
 console.log(val);
-console.log(typeof val);
-//console.log(val.length);
-console.log(val.toFixed())
+
+
+
+
