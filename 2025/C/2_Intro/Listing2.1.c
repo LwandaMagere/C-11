05f1e0fced010ec4e5576e02295155167c7582c3
dadolenssen@gmail.com <dadolenssen@gmail.com>
2025-09-17 12:02:41 +0300

    r

--- 2025/C/2_Intro/Listing2.1.c
+++ 2025/C/2_Intro/Listing2.1.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) 
 {
-    int num;        /* define a variable called num */
-    num = 1;        /* assign a value to num */
+    int num;
+    num = 1;
 
     printf("I am a simple ");
-    printf("Computer.\n");
+    printf("computer\n");
     printf("My favorite number is %d because it is first.\n", num);
-
-
+    
     return 0;
 }
