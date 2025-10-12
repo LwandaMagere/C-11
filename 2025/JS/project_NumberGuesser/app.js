666ce9040e1729177c51681fe6c9fec3af96e4cf
dadolenssen@gmail.com <dadolenssen@gmail.com>
2025-10-12 22:43:21 +0300

    r

---
+++ 2025/JS/project_NumberGuesser/app.js
@@ -0,0 +1,58 @@
+/**
+ * GAME FUNCTION:
+ * -Player must guess a number between a min and a max
+ * -Player gets a certain amount of guesses
+ * -Notify player of guesses remaining
+ * -Let player choose to play again
+ */
+
+// Game Values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+// Assign UI min n max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+
+    // console.log(typeof guessInput.value)
+
+    // Validate
+    if(isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    }
+
+    // Check if won
+    if(guess === winningNum) {
+        // Disable input
+        guessInput.disabled = true;
+        // Change border color
+        guessInput.style.borderColor = 'green';
+        setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+    } else {
+
+    }
+});
+
+// Set message
+function setMessage(msg, color) {
+    message.textContent = msg;
+    message.style.color = color;
+}
+
+/**
+ * isNaN()
+ */
