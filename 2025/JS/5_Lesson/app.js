d69c94bac6baa142dac2c8a2a455c4d9f226e6a5
dadolenssen@gmail.com <dadolenssen@gmail.com>
2025-10-14 08:40:19 +0300

    r

---
+++ 2025/JS/5_Lesson/app.js
@@ -0,0 +1,14 @@
+// Person Constructor
+function Person(name) {
+    this.name = name;
+    // console.log(this);
+}
+
+// console.log(this);
+// this.alert('hi')
+
+const brad = new Person('Brad');
+const john = new Person('John');
+
+console.log(brad);
+console.log(john);
