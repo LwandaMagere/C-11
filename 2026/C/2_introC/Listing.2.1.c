a2185e4b52eb88b3a7cdbda79944a70baf58447d
dadolenssen@gmail.com <dadolenssen@gmail.com>
2026-01-02 07:08:42 +0300

    first prog

---
+++ 2026/C/2_introC/Listing.2.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) /* A simple program*/
+{
+    int num;
+    num = 1;
+
+    printf("I am a simple ");
+    printf("Computer.\n");
+    printf("My favourite number is %d because it is first.\n", num);
+    
+    return 0;
+}
