50af0b6570d5416971a46ceefac2fb8eeb8701f1
dadolenssen@gmail.com <dadolenssen@gmail.com>
2026-01-02 07:56:09 +0300

    r

---
+++ 2026/C/2_introC/Listing.2.3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void butler(void);
+
+int main(void) 
+{
+    printf("I will surmone the butler function.\n");
+    butler();
+    printf("Yes. Bring me some tea and writeable DVDs.\n");
+    
+    return 0;
+}
+
+void butler(void) 
+{
+    printf("You rang, sir?\n");
+}
