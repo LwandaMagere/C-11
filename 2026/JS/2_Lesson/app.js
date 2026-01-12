c51128ed8340577e652713f879082ea07ab74b09
dadolenssen@gmail.com <dadolenssen@gmail.com>
2026-01-12 11:55:18 +0300

    r

---
+++ 2026/JS/2_Lesson/app.js
@@ -0,0 +1,34 @@
+// 1.TEMPLATE LITERALS
+// 2.ARRAY METHODS
+// 3.OBJECT LITERALS
+// 4.DATE N TIME
+// 5.IF STATEMENT
+// 6.FUNCTIONS
+// 7.LOOPS N ITERATIONS
+// 8.WINDOW OBJECT
+// 9.SCOPE
+
+// 1. TEMPLATE LITERALS
+
+// const name = 'John';
+// const age = 30;
+// const job = 'web developer';
+// const city = 'Miami';
+// let html;
+
+// function hello(){
+//     return 'Hello';
+// }
+
+// With template strings
+// html = `
+//     <ul>
+//         <li>Name: ${name}</li>
+//         <li>Age: ${age}</li>
+//         <li>City: ${city}</li>
+//         <li>Job: ${job}</li>
+//         <li>${2 + 2}</li>
+//         <li>${hello()}</li>
+//         <li>${age > 30 ? 'over 30' : 'under 30'}</li>
+//     </ul>
+// `
