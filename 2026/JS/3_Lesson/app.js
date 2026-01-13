f84d24a8e3dee0031c574a8a1eea0c743ea6ff28
dadolenssen@gmail.com <dadolenssen@gmail.com>
2026-01-13 15:23:26 +0300

    r

--- 2026/JS/3_Lesson/app.js
+++ 2026/JS/3_Lesson/app.js
@@ -7,3 +7,46 @@
  *  6. CLASSESS AND ATTRIBUTES
  *  7. EVENT LISTENERS, THE EVENT OBJECT
  */
+
+// 1. WHAT IS THE DOM (Document Object Model) ?
+
+/* 
+
+-A structured representation of an html document.
+-Tree of nodes/elements created by the browser.
+-Object Oriented representation
+-Javascript can be used to read/write/manipulate the dom
+
+-Properties of the DOM
+-Window object is the global object of the browser environment
+-Document is a property of window object.
+
+*/
+
+let val;
+
+// val = document;
+// val = document.all;
+// val = document.all[9];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.URL;
+// val = document.domain;
+// val = document.characterSet;
+// val = document.contentType;
+// val = window.document;
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].id;
+// val = document.links[0].className;
+// val = document.links[0].classList;
+// val = document.links[0].classList[0];
+
+// console.log(val);
