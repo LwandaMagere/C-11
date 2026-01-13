99317fcf84443ded185f2d0117b1872a7492f82b
dadolenssen@gmail.com <dadolenssen@gmail.com>
2026-01-13 15:36:06 +0300

    r

--- 2026/JS/3_Lesson/app.js
+++ 2026/JS/3_Lesson/app.js
@@ -23,7 +23,7 @@
 
 */
 
-let val;
+// let val;
 
 // val = document;
 // val = document.all;
@@ -49,4 +49,49 @@ let val;
 // val = document.links[0].classList;
 // val = document.links[0].classList[0];
 
+// val = document.images;
+// val = document.scripts;
+// val = document.scripts[2].getAttribute('src');
+
 // console.log(val);
+
+////// ***** END 1 ******* /////
+
+
+/* 2.DOM SELECTORS FOR SINGLE ELEMENTS */
+
+// 2.1 document.getElementById() // In the whole document grab me an element with an id of task-title
+// console.log(document.getElementById('task-title'));
+// // -- Get things from the element
+// console.log(document.getElementById('task-title').id);
+
+// You can put it in a variable to keep the code cleaner
+// const taskTitle = document.getElementById('task-title'); 
+
+// // -- Change styling
+// document.getElementById('task-title').style.background = '#333'; 
+// document.getElementById('task-title').style.color = '#fff';
+// document.getElementById('task-title').style.padding = '5px';
+// document.getElementById('task-title').style.display = 'none';
+
+// // --Change content
+// document.getElementById('task-title').textContent = 'Task List';
+// document.getElementById('task-title').innerText = 'My Tasks';
+// document.getElementById('task-title').innerHTML = '<span style="color:red">Task List</span>';
+
+// -- you can select any element with querySelector
+// 2.2 document.querySelector()
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
+
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'red';
+// document.querySelector('li:last-child').style.color = 'red'; 
+// document.querySelector('li:nth-child(3)').style.color = 'green'
+// document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+// document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+// document.querySelector('li:nth-child(even)').style.background = '#f4f4'
+
+
+////// ***** END 2.1 ******* /////
