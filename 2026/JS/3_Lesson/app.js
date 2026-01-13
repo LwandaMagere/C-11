fc89c3efce957137ab7af8719b42f8fb3c8bdb35
dadolenssen@gmail.com <dadolenssen@gmail.com>
2026-01-13 15:57:23 +0300

    R

--- 2026/JS/3_Lesson/app.js
+++ 2026/JS/3_Lesson/app.js
@@ -226,3 +226,47 @@
  */
 
 ////// ***** END 4  ******* /////
+
+// 5.CREATING ELEMENTS ADD INTO IT AND INSERT INTO THE DOM
+
+// // Create element
+// const li = document.createElement('li');
+
+// // Add class
+// li.className = 'collection-item';
+
+// // Add id
+// li.id = 'new-item';
+
+// // Add attribute
+// li.setAttribute('title', 'New Item');
+
+// //Create Text node and Append
+// li.appendChild(document.createTextNode('Hello World'));
+
+// // Create new link element
+// const link = document.createElement('a');
+
+// // Add class
+// link.className = 'delete-item secondary-content';
+
+// // Add icon html
+// link.innerHTML = '<i class="fa fa-remove"></i>';
+
+// // Append link into li
+// li.appendChild(link);
+
+// // Append li as child to ul
+// document.querySelector('ul.collection').appendChild(li);
+
+// console.log(li);
+
+/**
+ * 1.createElement('');
+ * 2.setAttribute('title', 'New Item');
+ * 3.createTextNode('Hello World');
+ * 4.appendChild()
+ */
+
+////// ***** END 5  ******* /////
+
