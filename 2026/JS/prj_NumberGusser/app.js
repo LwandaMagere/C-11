7a428d307e91ccb05b60498a9a4026b208529deb
dadolenssen@gmail.com <dadolenssen@gmail.com>
2026-01-30 09:20:37 +0300

    r

--- 2026/JS/prj_NumberGusser/app.js
+++ 2026/JS/prj_NumberGusser/app.js
@@ -35,15 +35,48 @@ guessBtn.addEventListener('click', function(){
 
     // Check if won
     if(guess === winningNum) {
-        // Disable input
-        guessInput.disabled = true;
-        // Change border color
-        guessInput.style.borderColor = 'green';
-        // Set message
-        setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+        // Game over won
 
+       gameOver(true, `${winningNum} is correct, YOU  WIN!`);
+
+    } else {
+        // Wrong number
+        guesseleft -= 1;
+
+        if(guesseleft === 0) {
+            // Game over lost
+
+            gameOver(false, `Game Over you lost. The correct number was ${winningNum}`,);
+
+        } else {
+            // Game continues - answer wrong
+            
+            // Change border color
+            guessInput.style.borderColor = 'red';
+
+            // Clear input
+            guessInput.value = '';
+
+            setMessage(`${guess} is not correct, ${guesseleft} guesses left`, 'red');
+        }
     }
 });
+
+// Game over
+function gameOver(won, msg) {
+    let color;
+    won === true ? color = 'green' : 'red';
+
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = color;
+    // Set text color
+    message.style.color = color;
+    // Set message
+    setMessage(msg);
+
+}
 
 // Set message
 function setMessage(msg, color) {
