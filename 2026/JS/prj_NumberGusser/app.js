76f8669a82397cdc6cc54d0f1704f250a85fdbd9
dadolenssen@gmail.com <dadolenssen@gmail.com>
2026-01-30 09:39:43 +0300

    r

--- 2026/JS/prj_NumberGusser/app.js
+++ 2026/JS/prj_NumberGusser/app.js
@@ -9,7 +9,7 @@
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(),
     guesseleft = 3;
 
 // UI Elements
@@ -23,6 +23,13 @@ const game = document.querySelector('#game'),
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again') {
+        window.location.reload();
+    }
+})
 
 // Listen for guess
 guessBtn.addEventListener('click', function(){
@@ -76,6 +83,15 @@ function gameOver(won, msg) {
     // Set message
     setMessage(msg);
 
+    // Play Again?
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
+
+}
+
+// Get winning number
+function getRandomNum() {
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 // Set message
