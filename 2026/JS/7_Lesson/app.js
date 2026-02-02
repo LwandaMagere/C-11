213a3c060d4f137fb9aed6eb470404dcf0318188
dadolenssen@gmail.com <dadolenssen@gmail.com>
2026-02-02 22:04:17 +0300

    r

--- 2026/JS/7_Lesson/app.js
+++ 2026/JS/7_Lesson/app.js
@@ -1,44 +1,7 @@
-const posts = [
-    {title: 'Post one', body: 'This is post one'},
-    {title: 'Post two', body: 'This is post two'}
-];
+// const http = new EasyHTTP;
+const http = new EasyHTTP;
 
-// function createPost(post) {
-//     setTimeout(function() {
-//         posts.push(post);
-//     }, 2000);
-// }
-
-// function getPosts() {
-//     setTimeout(function() {
-//         let output = '';
-//         posts.forEach(function(post){
-//             output += `<li>${post.title}</li>`;  
-//         })
-//         document.body.innerHTML = output;
-//     }, 1000);
-// };
+// Get Users
+http.get('https://json.placeholder.typicode.com/user')
 
 
-function createPost(post, callback) {
-    setTimeout(function() {
-        posts.push(post);
-        callback();
-    }, 2000);
-}
-
-function getPosts() {
-    setTimeout(function() {
-        let output = '';
-        posts.forEach(function(post){
-            output += `<li>${post.title}</li>`;  
-        })
-        document.body.innerHTML = output;
-    }, 1000);
-};
-
-// createPost({title:'Post three', body: 'This is post threee'});
-
-// getPosts();
-
-createPost({title:'Post three', body: 'This is post threee'}, getPosts);
