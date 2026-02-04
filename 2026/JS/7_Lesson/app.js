73464cd211c2e583e9e54a992cfdb82b7440ebaa
dadolenssen@gmail.com <dadolenssen@gmail.com>
2026-02-04 12:19:11 +0300

    r

--- 2026/JS/7_Lesson/app.js
+++ 2026/JS/7_Lesson/app.js
@@ -1,12 +1,36 @@
-// Call back server demonstration
+// Call back server demonstration synchronous
 const posts = [
     {title: 'Post One', body: 'This is post one'},
     {title: 'Post Two', body: 'This is post two'}
 ]
 
-function createPost(post) {
+// function createPost(post) {
+//     setTimeout(function(){
+//         posts.push(post);
+//     }, 2000)
+// }
+
+// function getPosts() {
+//     setTimeout(function() {
+//         let output = '';
+//         posts.forEach(function(post) {
+//             output += `<li>${post.title}</li>`;
+//         });
+//         // console.log(output);
+//         document.getElementById('test').innerHTML = output;
+//     }, 1000);
+// }
+
+// getPosts();
+
+// createPost({title: 'Post Three', body: 'This is post three'});
+
+
+// Call back server demonstration Asynchronous
+function createPost(post, callback) {
     setTimeout(function(){
         posts.push(post);
+        callback();
     }, 2000)
 }
 
@@ -21,7 +45,5 @@ function getPosts() {
     }, 1000);
 }
 
-getPosts();
-
-createPost({title: 'Post Three', body: 'This is post three'});
+createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
 
