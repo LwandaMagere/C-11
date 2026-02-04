3d7d5ac4cae088a6491acb9b13c41d0745a60653
dadolenssen@gmail.com <dadolenssen@gmail.com>
2026-02-04 15:51:37 +0300

    r

--- 2026/JS/7_Lesson/app.js
+++ 2026/JS/7_Lesson/app.js
@@ -27,11 +27,43 @@ const posts = [
 
 
 // Call back server demonstration Asynchronous
-function createPost(post, callback) {
-    setTimeout(function(){
-        posts.push(post);
-        callback();
-    }, 2000)
+// function createPost(post, callback) {
+//     setTimeout(function(){
+//         posts.push(post);
+//         callback();
+//     }, 2000)
+// }
+
+// function getPosts() {
+//     setTimeout(function() {
+//         let output = '';
+//         posts.forEach(function(post) {
+//             output += `<li>${post.title}</li>`;
+//         });
+//         // console.log(output);
+//         document.getElementById('test').innerHTML = output;
+//     }, 1000);
+// }
+
+// createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+
+
+// PROMISES
+function createPost(post) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function(){
+            posts.push(post);
+            const error = true;
+
+            if(!error) {
+                resolve();
+            }else {
+                reject('Error: Something went wrong!')
+            }
+
+
+        }, 2000)
+    })
 }
 
 function getPosts() {
@@ -45,5 +77,8 @@ function getPosts() {
     }, 1000);
 }
 
-createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+createPost({title: 'Post Three', body: 'This is post three'})
+.then(getPosts)
+.catch( err => console.log(err));
+
 
--- 2026/JS/fetch_API/app.js
+++ 2026/JS/fetch_API/app.js
@@ -6,6 +6,55 @@ document.getElementById('button2').addEventListener('click', getJson);
 
 document.getElementById('button3').addEventListener('click', getExternal);
 
+function getText() {
+    fetch('test.txt')
+    .then(function(res) {
+        // console.log(res); // show the returned promise
+        // console.log(res.text());
+        return res.text();
+    }).then(function(data){
+        console.log(data);
+        document.getElementById('output').innerHTML = data;
+    }).catch(function(err) {
+        console.log(err);
+    })
+}
+
+
+function getJson() {
+    fetch('posts.json')
+    .then(function(res) {
+        return res.json();
+    }).then(function(data){
+        console.log(data);
+        let output = '';
+        data.forEach(function(post) {
+            output += `<li>${post.title}</li>`
+        })
+        document.getElementById('output').innerHTML = output;
+    }).catch(function(err) {
+        console.log(err);
+    })
+}
+
+function getExternal() {
+    fetch('posts2.json')
+    .then(function(res) {
+        return res.json();
+    }).then(function(data){
+        console.log(data);
+        let output = '';
+        data.forEach(function(post) {
+            output += `<li>${post.login}</li>`
+        })
+        document.getElementById('output').innerHTML = output;
+    }).catch(function(err) {
+        console.log(err);
+    })
+}
+
+
+
 
 
 
